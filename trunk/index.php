306beab4b18204e471a5ef74a9be9018c784d3c3
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2011-02-17 14:47:45 +0000

    черновой вариант движка LOCA

--- trunk/index.php
+++ trunk/index.php
@@ -1,1 +1,16 @@
-Hallo.
+<?
+
+require_once "loca.php";
+
+//loca_reset ( "localhost", "toor", "qwerty", "ogame" );
+
+loca_init ( "localhost", "toor", "qwerty", "ogame", "ru" );
+//loca_add ("LOCA_BLABLABLA", "1");
+//loca_add ("LOCA_BLABLABLA", "2");
+//loca_add ("LOCA_BLABLABLA", "LATS!");
+
+echo loca("LOCA_BLABLABLA");
+
+?>
+
+OK.
