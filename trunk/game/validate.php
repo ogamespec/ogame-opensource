7ab87b1b8e4c89d6becf761d377e1fe64ed91c50
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2011-04-14 13:24:04 +0000

    validation fixed

---
+++ trunk/game/validate.php
@@ -0,0 +1,39 @@
+<?php
+
+// Активация аккаунта по ссылке.
+
+require_once "config.php";
+require_once "db.php";
+
+// Соединиться с базой данных
+dbconnect ($db_host, $db_user, $db_pass, $db_name);
+dbquery("SET NAMES 'utf8';");
+dbquery("SET CHARACTER SET 'utf8';");
+dbquery("SET SESSION collation_connection = 'utf8_general_ci';");
+
+require_once "loca_game.php";
+require_once "bbcode.php";
+require_once "uni.php";
+require_once "prod.php";
+require_once "planet.php";
+require_once "user.php";
+require_once "msg.php";
+require_once "notes.php";
+require_once "queue.php";
+require_once "page.php";
+require_once "ally.php";
+require_once "unit.php";
+require_once "fleet.php";
+require_once "battle.php";
+require_once "debug.php";
+
+function RedirectHome ()
+{
+    global $StartPage;
+    echo "<html><head><meta http-equiv='refresh' content='0;url=$StartPage' /></head><body></body>";
+}
+
+if ( key_exists("ack", $_GET) ) ValidateUser ($_GET['ack']);
+else RedirectHome ();
+
+?>
