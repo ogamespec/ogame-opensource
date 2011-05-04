0f634c1b3bf0cc991e767bd5494a537ca592fa5b
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2011-05-04 13:13:22 +0000

    mega update

---
+++ game/expedition.php
@@ -0,0 +1,17 @@
+<?php
+
+// Экспедиции.
+
+function ExpeditionArrive ($queue, $fleet_obj, $fleet)
+{
+}
+
+function ExpeditionHold ($queue, $fleet_obj, $fleet)
+{
+}
+
+function ExpeditionReturn ($queue, $fleet_obj, $fleet)
+{
+}
+
+?>
