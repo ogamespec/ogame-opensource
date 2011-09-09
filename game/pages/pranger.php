3bf948a8fa8adaef187fb0e4ce362c8a02039c10
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2011-09-09 07:07:14 +0000

    + errorpage

--- game/pages/pranger.php
+++ game/pages/pranger.php
@@ -1,17 +1,3 @@
 <?php
 
-if (CheckSession ( $_GET['session'] ) == FALSE) die ();
-if ( key_exists ('cp', $_GET)) SelectPlanet ($GlobalUser['player_id'], $_GET['cp']);
-$now = time();
-UpdateQueue ( $now );
-$aktplanet = GetPlanet ( $GlobalUser['aktplanet'] );
-ProdResources ( $GlobalUser['aktplanet'], $aktplanet['lastpeek'], $now );
-UpdatePlanetActivity ( $aktplanet['planet_id'] );
-UpdateLastClick ( $GlobalUser['player_id'] );
-$session = $_GET['session'];
-
-PageHeader ("pranger");
-
-PageFooter ();
-ob_end_flush ();
 ?>
