543c86d68c8e9b078cb4bc30a7c3957ce22b8243
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2012-02-08 12:57:14 +0000

    - bot API

--- game/bot.php
+++ game/bot.php
@@ -2,4 +2,36 @@
 
 // Управление ботами.
 
+// Интеллект ботов - секретная инфорация.
+require_once "bot_rev1.php";
+
+// Запустить бота. Для каждой планеты бота выбирается оптимальная стратегия развития.
+function StartBot ($player_id)
+{
+    StartBot_Rev1 ($player_id);
+}
+
+// Остановить бота (просто удалить все задания AI)
+function StopBot ($player_id)
+{
+    $global $db_prefix;
+    $query = "DELETE FROM ".$db_prefix."queue WHERE type = 'AI' AND owner_id = $player_id";
+    dbquery ($query);
+}
+
+// Проверить является ли игрок ботом.
+function IsBot ($player_id)
+{
+    $global $db_prefix;
+    $query = "SELECT * FROM ".$db_prefix."queue WHERE type = 'AI' AND owner_id = $player_id";
+    $result = dbquery ($query);
+    return ( dbrows ($result) > 0 ) ;
+}
+
+// Событие завершения заданий для бота. Вызывается из queue.php
+function Queue_Bot_End ($queue)
+{
+    Queue_Bot_End_Rev1 ();
+}
+
 ?>
