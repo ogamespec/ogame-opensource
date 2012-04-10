44b2e4f42f673b3d0071a273bbded070cf62d380
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2012-04-10 09:42:13 +0000

    - removed bbcode for debug/error messages

--- game/debug.php
+++ game/debug.php
@@ -11,7 +11,7 @@ function Error ($text)
         $GlobalUser['player_id'] = 0;
     }
 
-    $text = str_replace ( '\"', "&quot;", bb($text) );
+    $text = str_replace ( '\"', "&quot;", ($text) );
     $text = str_replace ( '\'', "&rsquo;", $text );
     $text = str_replace ( '\`', "&lsquo;", $text );
 
@@ -45,7 +45,7 @@ function Debug ($message)
     global $GlobalUser;
     if ( !$GlobalUser ) return;
 
-    $message = str_replace ( '\"', "&quot;", bb($message) );
+    $message = str_replace ( '\"', "&quot;", ($message) );
     $message = str_replace ( '\'', "&rsquo;", $message );
     $message = str_replace ( '\`', "&lsquo;", $message );
 
