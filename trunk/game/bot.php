b0f109b79271d59f629b220cbd28427b1a0191ad
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2012-04-20 12:57:14 +0000

    - !!! NEW TABLE !!! Bot variables.

--- trunk/game/bot.php
+++ trunk/game/bot.php
@@ -45,4 +45,39 @@ function Queue_Bot_End ($queue)
     RemoveQueue ($queue['task_id']);
 }
 
+// Переменные бота​.
+
+function GetVar ( $owner_id, $var, $def_value=null )
+{
+    global $db_prefix;
+    $query = "SELECT * FROM ".$db_prefix."botvars WHERE var = '".$var."' AND owner_id = $owner_id LIMIT 1;";
+    $result = dbquery ($query);
+    if ( dbrows ($result) > 0 ) {
+        $var = dbarray ( $result );
+        return $var['value'];
+    }
+    else
+    {
+        $var = array ( '', $owner_id, $var, $def_value );
+        AddDBRow ( $var, 'botvars' );
+        return $def_value;
+    }
+}
+
+function SetVar ( $owner_id, $var, $value )
+{
+    global $db_prefix;
+    $query = "SELECT * FROM ".$db_prefix."botvars WHERE var = '".$var."' AND owner_id = $owner_id LIMIT 1;";
+    $result = dbquery ($query);
+    if ( dbrows ($result) > 0 ) {
+        $query = "UPDATE ".$db_prefix."botvars SET value = '".$value."' WHERE var = '".$var."' AND owner_id = $owner_id;";
+        dbquery ($query);
+    }
+    else
+    {
+        $var = array ( '', $owner_id, $var, $value );
+        AddDBRow ( $var, 'botvars' );
+    }
+}
+
 ?>
