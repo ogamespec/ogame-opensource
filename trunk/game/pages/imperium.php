9ee0fa18ac741659d213a85d3a9ca6205372dfea
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2012-04-25 09:56:11 +0000

    - update planet production in empire

--- trunk/game/pages/imperium.php
+++ trunk/game/pages/imperium.php
@@ -38,11 +38,12 @@ if ( $planettype == 1 || $planettype == 3)
         if ( $planettype == 1 && $planet['type'] == 0 ) continue;
         if ( $planettype == 3 && $planet['type'] != 0 ) continue;
         $plist[$num] = GetPlanet ($planet['planet_id']);
+        ProdResources ( &$plist[$num], $plist[$num]['lastpeek'], $now );
         $num ++;
     }
 }
 
-$unitab = LoadUniverse ( );
+$unitab = $GlobalUni;
 $speed = $unitab['speed'];
 
 $buildmap = array ( 1, 2, 3, 4, 12, 14, 15, 21, 22, 23, 24, 31, 33, 34, 41, 42, 43, 44 );
