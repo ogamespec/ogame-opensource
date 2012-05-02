6ae48a01378de5525636feb9be6ff05a46e3e90d
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2012-05-02 15:32:37 +0000

    - more loca

--- trunk/game/loca/menu.php
+++ trunk/game/loca/menu.php
@@ -33,6 +33,31 @@
     }
 
     else if ( $lang === "en" ) {
+        $LOCA["en"]["MENU_OVERVIEW"] = "Overview";
+        $LOCA["en"]["MENU_ADMIN"] = "*Admin Area*";
+        $LOCA["en"]["MENU_EMPIRE"] = "Empire";
+        $LOCA["en"]["MENU_BUILDING"] = "Buildings";
+        $LOCA["en"]["MENU_RESOURCES"] = "Resources";
+        $LOCA["en"]["MENU_TRADER"] = "Merchant";
+        $LOCA["en"]["MENU_RESEARCH"] = "Research";
+        $LOCA["en"]["MENU_SHIPYARD"] = "Shipyard";
+        $LOCA["en"]["MENU_FLEET"] = "Fleet";
+        $LOCA["en"]["MENU_TECHTREE"] = "Technology";
+        $LOCA["en"]["MENU_GALAXY"] = "Galaxy";
+        $LOCA["en"]["MENU_DEFENSE"] = "Defense";
+        $LOCA["en"]["MENU_ALLY"] = "Alliance";
+        $LOCA["en"]["MENU_BOARD"] = "Board";
+        $LOCA["en"]["MENU_PAYMENT"] = "Officers Recruitment";
+        $LOCA["en"]["MENU_STAT"] = "Statistics";
+        $LOCA["en"]["MENU_SEARCH"] = "Search";
+        $LOCA["en"]["MENU_TUTORIAL"] = "Help";
+        $LOCA["en"]["MENU_MESSAGES"] = "Messages";
+        $LOCA["en"]["MENU_NOTES"] = "Notes";
+        $LOCA["en"]["MENU_BUDDY"] = "Buddylist";
+        $LOCA["en"]["MENU_OPTIONS"] = "Options";
+        $LOCA["en"]["MENU_LOGOUT"] = "Logout";
+        $LOCA["en"]["MENU_RULES"] = "Rules";
+        $LOCA["en"]["MENU_IMPRESSUM"] = "Impressum";
     }
 
     else if ( $lang === "ru" ) {
