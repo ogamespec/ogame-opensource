45c070a70b82d64eff823f066039a7e1fb183d77
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2012-12-20 14:54:36 +0000

    - loca: search

---
+++ trunk/game/loca/ru_ru/search.php
@@ -0,0 +1,24 @@
+<?php
+
+// Поиск
+
+$LOCA["ru"]["SEARCH_SEARCH"] = "Поиск";
+$LOCA["ru"]["SEARCH_SEL_USER"] = "Имя игрока";
+$LOCA["ru"]["SEARCH_SEL_PLANET"] = "Название планеты";
+$LOCA["ru"]["SEARCH_SEL_TAG"] = "Аббревиатура альянса";
+$LOCA["ru"]["SEARCH_SEL_ALLY"] = "Название альянса";
+$LOCA["ru"]["SEARCH_BUTTON"] = "Искать";
+$LOCA["ru"]["SEARCH_NAME"] = "Имя/Название";
+$LOCA["ru"]["SEARCH_ALLY"] = "Альянс";
+$LOCA["ru"]["SEARCH_PLANET"] = "Планета";
+$LOCA["ru"]["SEARCH_COORDS"] = "Координаты";
+$LOCA["ru"]["SEARCH_POINTS"] = "Очки";
+$LOCA["ru"]["SEARCH_TAG"] = "Аббревиатура";
+$LOCA["ru"]["SEARCH_MEMBERS"] = "Члены";
+$LOCA["ru"]["SEARCH_MESSAGE"] = "Послать сообщение";
+$LOCA["ru"]["SEARCH_BUDDY"] = "Предложение подружиться";
+$LOCA["ru"]["SEARCH_NORESULT"] = "Ничего не найдено";
+$LOCA["ru"]["SEARCH_MAX_USERS_PLANETS"] = "Найдено больше, чем #1 результатов";
+$LOCA["ru"]["SEARCH_MAX_ALLY"] = "Найдено больше, чем #1 сообщений";
+
+?>
