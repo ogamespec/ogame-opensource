6a2afc1b06b16f56e3a9b5670d6abfe13001211f
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2012-12-21 06:00:38 +0000

    - loca: search (de)

---
+++ trunk/game/loca/de_de/search.php
@@ -0,0 +1,25 @@
+<?php
+
+// Suche
+
+$LOCA["de"]["SEARCH_SEARCH"] = "OGame Universum durchsuchen";
+$LOCA["de"]["SEARCH_SEL_USER"] = "Spielername";
+$LOCA["de"]["SEARCH_SEL_PLANET"] = "Planetname";
+$LOCA["de"]["SEARCH_SEL_TAG"] = "Allianz Tag";
+$LOCA["de"]["SEARCH_SEL_ALLY"] = "Allianz Name";
+$LOCA["de"]["SEARCH_BUTTON"] = "Los gehts!";
+$LOCA["de"]["SEARCH_NAME"] = "Name";
+$LOCA["de"]["SEARCH_ALLY"] = "Ally";
+$LOCA["de"]["SEARCH_PLANET"] = "Planet";
+$LOCA["de"]["SEARCH_COORDS"] = "Pos";
+$LOCA["de"]["SEARCH_PLACE"] = "Platz";
+$LOCA["de"]["SEARCH_POINTS"] = "Punkte";
+$LOCA["de"]["SEARCH_TAG"] = "Tag";
+$LOCA["de"]["SEARCH_MEMBERS"] = "Mitglieder";
+$LOCA["de"]["SEARCH_MESSAGE"] = "Nachricht schreiben";
+$LOCA["de"]["SEARCH_BUDDY"] = "Buddyanfrage";
+$LOCA["de"]["SEARCH_NORESULT"] = "Es wurden leider keine Eintr&auml;ge gefunden";
+$LOCA["de"]["SEARCH_MAX_USERS_PLANETS"] = "Es wurden mehr als #1 Eintr&auml;ge gefunden";
+$LOCA["de"]["SEARCH_MAX_ALLY"] = "Es wurden mehr als #1 Suche";
+
+?>
--- trunk/game/loca/en_en/search.php
+++ trunk/game/loca/en_en/search.php
@@ -12,6 +12,7 @@ $LOCA["en"]["SEARCH_NAME"] = "Name";
 $LOCA["en"]["SEARCH_ALLY"] = "Alliance";
 $LOCA["en"]["SEARCH_PLANET"] = "Planet";
 $LOCA["en"]["SEARCH_COORDS"] = "Position";
+$LOCA["en"]["SEARCH_PLACE"] = "Place";
 $LOCA["en"]["SEARCH_POINTS"] = "Points";
 $LOCA["en"]["SEARCH_TAG"] = "Tag";
 $LOCA["en"]["SEARCH_MEMBERS"] = "Member";
