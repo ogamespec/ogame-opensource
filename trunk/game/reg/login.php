d7b0e1278ab719c44c95a8ffa3366da6a8963684
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2013-02-21 08:53:36 +0000

    - original game internal login page

---
+++ trunk/game/reg/login.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+<title>Galactic Empires - ogame.de</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="formate.css">
+</head>
+
+<body bgcolor="#000000" text="#00FF00">
+<div align="center">
+ <p>&nbsp;</p>
+<form name="ogame" method="post" action="login2.php">
+<input type="hidden" name="v" value="2">
+   <table width="801" border="0" cellspacing="0" cellpadding="0">
+     <tr>
+       <td height="602" background="stinger-me.jpg" align="left" valign="top">
+         <div align="center">
+           <table width="100%" border="0" cellspacing="0" cellpadding="0" height="66%">
+             <tr>
+               <td width="44%" height="73">&nbsp;</td>
+               <td width="17%" height="73">&nbsp;</td>
+               <td width="39%" height="73">&nbsp;</td>
+             </tr>
+             <tr>
+               <td width="44%" height="42">&nbsp;</td>
+               <td width="17%" height="42">
+                 <div align="right"><font color="#00CC00"><b><font face="Tahoma" size="2">Login:</font></b></font></div>
+               </td>
+               <td width="39%" height="42"> &nbsp;&nbsp;
+                 <input type="text" name="login" class="input">
+               </td>
+             </tr>
+             <tr>
+               <td width="44%" height="2">&nbsp;</td>
+               <td width="17%" height="2">
+                 <div align="right"><font color="#00CC00"><b><font face="Tahoma" size="2">Password:</font></b></font></div>
+               </td>
+               <td width="39%" height="2"> &nbsp;&nbsp;
+                 <input type="password" name="pass" class="input">
+               </td>
+             </tr>
+             <tr>
+               <td height="35" width="44%">&nbsp;</td>
+               <td height="35" width="17%">&nbsp;</td>
+               <td height="35" width="39%">
+                 <div align="left"> &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                   <input type="submit" name="Abschicken" value="Login" class="button">
+                 </div>
+               </td>
+             </tr>
+             <tr>
+               <td height="400" width="44%">&nbsp;</td>
+               <td height="400" width="17%">&nbsp;</td>
+               <td height="400" width="39%">&nbsp;</td>
+             </tr>
+           </table>
+       </div>
+     </td>
+   </tr>
+   <tr>
+     <td>
+         <div align="center"><font face="Tahoma" size="1"><b><font color="#00CC00" size="2">Wenn
+           du dich Neu Anmelden m&ouml;chtest klicke bitte hier auf <a href="new.php" target="_blank">Registrieren</a>
+           .</font></b></font></div>
+       </td>
+   </tr>
+ </table>  
+ </form>
+ <p>&nbsp;</p>
+</div>
+
+</body>
+</html>
+
