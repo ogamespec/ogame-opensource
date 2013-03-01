abe03b3ac9f9aa39a3a0e4c834d6725b6f2627f7
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2013-03-01 07:39:09 +0000

    - feed archived

---
+++ trunk/feed/show-example.php
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="UTF-8"?> 
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>OGame-Nachrichten von andor</title>
+		<link>http://uni20.ogame.de/feed/show.php?feedid=5aa28084f43ad54d9c8f7dd92f774d03</link>
+		<atom:link href="http://uni20.ogame.de/feed/show.php?feedid=5aa28084f43ad54d9c8f7dd92f774d03" rel="self" type="application/rss+xml" />
+		<description>Kampfberichte, Spionagereports und Systemmeldungen des OGame-Accounts von andor</description>
+		<language>de-de</language>
+		<pubDate>Thu, 28 Feb 2013 22:07:35 +0100
+</pubDate>
+		<item>
+			<title>Produktion abgebrochen</title>
+			<description>
+				<![CDATA[
+					Der Bauauftrag für dein Gebäude Roboterfabrik Stufe 3 auf Planet Kolonie [3:280:4] konnte nicht in Auftrag gegeben werden.<br><br>Du hast nicht genug Resourcen !				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92751237&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92751237.5aa28084f43ad54d9c8f7dd92f774d03.1362055021.i</guid>
+			<pubDate>Thu, 28 Feb 2013 13:37:01 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Produktion abgebrochen</title>
+			<description>
+				<![CDATA[
+					Der Bauauftrag für dein Gebäude Roboterfabrik Stufe 3 auf Planet Kolonie [3:280:4] konnte nicht in Auftrag gegeben werden.<br><br>Du hast nicht genug Resourcen !				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92751236&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92751236.5aa28084f43ad54d9c8f7dd92f774d03.1362055021.i</guid>
+			<pubDate>Thu, 28 Feb 2013 13:37:01 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Kampfbericht [1:40:9] (V:0,A:0)</title>
+			<description>
+				<![CDATA[
+					Folgende Flotten standen sich am 02-28 22:07:35 gegenüber:<br><table border=1 width=100%><tr><th><br><center>Angreifer Fabia82 ([1:36:6])<br>Waffen: 90% Schilde: 90% Hülle: 90% <table border=1><tr><th>Typ</th><th>Gr.Trans</th></tr><tr><th>Anz.</th><th>3</th></tr><tr><th>Bewaff.</th><th>10</th></tr><tr><th>Schilde</th><th>48</th></tr><tr><th>Hülle</th><th>2.280</th></tr></table></center></th></tr></table><table border=1 width=100%><tr><th><br><center>Verteidiger andor ([1:40:9])<br>Waffen: 40% Schilde: 40% Hülle: 40% <br>Vernichtet</center></th></th></tr></table><!--A:230253,W:230253--><p> Der Angreifer hat die Schlacht gewonnen!<br>Er erbeutet<br>38.408 Metall, 17.800 Kristall und 5.382 Deuterium<br><p><br>Der Angreifer hat insgesamt 0 Units verloren.<br>Der Verteidiger hat insgesamt 0 Units verloren.<br>Auf diesen Raumkoordinaten liegen nun 0 Metall und 0 Kristall. 				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92754010&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92754010.5aa28084f43ad54d9c8f7dd92f774d03.1362085655.i</guid>
+			<pubDate>Thu, 28 Feb 2013 22:07:35 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Stationierung einer Flotte</title>
+			<description>
+				<![CDATA[
+					Eine deiner Flotten (Kleiner Transporter: 1 ) hat Kolonie [1:40:14]  erreicht. Die Flotte liefert 3.000 Metall, 1.998 Kristall und 0 Deuterium<br>				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92751665&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92751665.5aa28084f43ad54d9c8f7dd92f774d03.1362061968.i</guid>
+			<pubDate>Thu, 28 Feb 2013 15:32:48 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Siedlungsbericht</title>
+			<description>
+				<![CDATA[
+					Die Flotte erreicht die angegebenen Koordinaten [1:40:14], findet dort einen neuen Planeten und beginnt sofort mit seiner Erschließung.				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92751242&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92751242.5aa28084f43ad54d9c8f7dd92f774d03.1362055178.i</guid>
+			<pubDate>Thu, 28 Feb 2013 13:39:38 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Produktion abgebrochen</title>
+			<description>
+				<![CDATA[
+					Der Bauauftrag für dein Gebäude Roboterfabrik Stufe 5 auf Planet Kolonie [3:280:5] konnte nicht in Auftrag gegeben werden.<br><br>Du hast nicht genug Resourcen !				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92751313&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92751313.5aa28084f43ad54d9c8f7dd92f774d03.1362056874.i</guid>
+			<pubDate>Thu, 28 Feb 2013 14:07:54 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Produktion abgebrochen</title>
+			<description>
+				<![CDATA[
+					Der Bauauftrag für dein Gebäude Roboterfabrik Stufe 5 auf Planet Kolonie [3:280:5] konnte nicht in Auftrag gegeben werden.<br><br>Du hast nicht genug Resourcen !				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92751314&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92751314.5aa28084f43ad54d9c8f7dd92f774d03.1362056874.i</guid>
+			<pubDate>Thu, 28 Feb 2013 14:07:54 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Produktion abgebrochen</title>
+			<description>
+				<![CDATA[
+					Der Bauauftrag für dein Gebäude Roboterfabrik Stufe 5 auf Planet Kolonie [3:280:5] konnte nicht in Auftrag gegeben werden.<br><br>Du hast nicht genug Resourcen !				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92751315&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92751315.5aa28084f43ad54d9c8f7dd92f774d03.1362056874.i</guid>
+			<pubDate>Thu, 28 Feb 2013 14:07:54 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Spionageaktion</title>
+			<description>
+				<![CDATA[
+					Eine fremde Flotte von Planet Kolonie [1:36:6]  wurde in der Nähe deines Planeten Planet [1:40:9] gesichtet. Chance auf Spionageabwehr: 0 %				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92753603&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92753603.5aa28084f43ad54d9c8f7dd92f774d03.1362080492.i</guid>
+			<pubDate>Thu, 28 Feb 2013 20:41:32 +0100
+</pubDate>
+		</item>
+		<item>
+			<title>Spionageaktion</title>
+			<description>
+				<![CDATA[
+					Eine fremde Flotte von Planet Kolonie [1:36:6]  wurde in der Nähe deines Planeten Kolonie [1:40:14] gesichtet. Chance auf Spionageabwehr: 0 %				
+				]]>
+			</description>
+			<link>http://uni20.ogame.de/feed/viewitem.php?mid=92753605&amp;feedid=5aa28084f43ad54d9c8f7dd92f774d03&amp;type=i</link>
+			<author>feedcommander.noreply@ogame.de (OGame Feed Commander)</author>
+			<guid isPermaLink="false">92753605.5aa28084f43ad54d9c8f7dd92f774d03.1362080506.i</guid>
+			<pubDate>Thu, 28 Feb 2013 20:41:46 +0100
+</pubDate>
+		</item>
+	</channel>
+</rss>
