3b397b92d9a06dc3215382441a41c8d1df701451
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2013-03-14 08:27:12 +0000

    - password recovery

--- trunk/game/reg/fa_pass.php
+++ trunk/game/reg/fa_pass.php
@@ -18,9 +18,12 @@ require_once "../planet.php";
 require_once "../bot.php";
 require_once "../user.php";
 require_once "../queue.php";
-require_once "../events.php";
 require_once "../uni.php";
 require_once "../debug.php";
+require_once "../loca.php";
+
+$loca_lang = $_COOKIE['ogamelang'];
+loca_add ( "reg", $loca_lang );
 
 function method () { return $_SERVER['REQUEST_METHOD']; }
 
@@ -43,39 +46,70 @@ function va ($subject)
     return preg_replace($pattern, $replace, $subject);
 }
 
-function gen_trivial_password ($len = 8)
+function gen_trivial_password ()
 {
-    $r = '';
-    for($i=0; $i<$len; $i++)
-        $r .= chr(rand(0, 25) + ord('a'));
-    return $r;
+    $pass = "";
+    $syllables = "er,in,tia,wol,fe,pre,vet,jo,nes,al,len,son,cha,ir,ler,bo,ok,tio,nar,sim,ple,bla,ten,toe,cho,co,lat,spe,ak,er,po,co,lor,pen,cil,li,ght,wh,at,the,he,ck,is,mam,bo,no,fi,ve,any,way,pol,iti,cs,ra,dio,sou,rce,sea,rch,pa,per,com,bo,sp,eak,st,fi,rst,gr,oup,boy,ea,gle,tr,ail,bi,ble,brb,pri,dee,kay,en,be,se";
+
+    $syllable_array = explode (",", $syllables);
+    srand ((double)microtime()*1000000);
+    for ($count=1; $count<=4; $count++) {
+        if (rand()%10 == 1) $pass .= sprintf ("%0.0f", (rand()%50)+1);
+        else $pass .= sprintf ("%s", $syllable_array[rand()%62]);
+    }
+    return $pass;
 }
 
-/*
-From: welcome@ogame.ru
-Subj: Пароль огейм
+function isValidEmail($email){
+	return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email);
+}
 
-"О xxx, приветствуем Вас!
+function EmailExist ( $email)
+{
+    global $db_prefix;
+    $email = mb_strtolower ($email, 'UTF-8');
+    $query = "SELECT * FROM ".$db_prefix."users WHERE (email = '".$email."' OR pemail = '".$email."')";
+    $result = dbquery ($query);
+    return dbarray ($result);
+}
 
-Попасть снова в xxx-ю вселенную Вы сможете только указав пароль xxx.
+// Соединиться с базой данных
+dbconnect ($db_host, $db_user, $db_pass, $db_name);
+dbquery("SET NAMES 'utf8';");
+dbquery("SET CHARACTER SET 'utf8';");
+dbquery("SET SESSION collation_connection = 'utf8_general_ci';");
 
-Пароли высылаются исключительно на адреса, указанные в профиле аккаунта.
-
-Если Вы не заказывали восстановление пароля, то проигнорируйте это письмо.
-
-Удачи,
-
-Ваша команда ОГейм."
-*/
+$uni = LoadUniverse ();
+$uninum = $uni['num'];
 
 $pass_ok = false;
+if ( method () === "POST" ) {
+    $email = $_POST['email'];
+    if ( isValidEmail ($email) ) {
+        $user = EmailExist ( $email );
+        if ( $user ) {
+            $pass = gen_trivial_password ();
+            $md5 = md5 ($pass . $db_secret );
+            $query = "UPDATE ".$db_prefix."users SET session = '', password = '".$md5."' WHERE player_id = " . $user['player_id'];
+            dbquery ($query);
+            mail_utf8 ( $user['pemail'], loca("REG_FORGOT_SUBJ"), 
+                va ( loca("REG_FORGOT_MAIL"), 
+                    $user['oname'], 
+                    $uninum, 
+                    $pass,
+                    "http://" . hostname()
+                ), "From: welcome@" . hostname() );
+            $pass_ok = true;
+        }
+    }
+}
 
 ?>
 
 <html> 
  <head> 
-  <title>Отправление пароля OGame</title> 
-<!--  <meta http-equiv="refresh" content="5; URL=http://ogame.de"> --> 
+  <title><?=loca("REG_FORGOT_TITLE");?></title> 
+<!--  <meta http-equiv="refresh" content="5; URL=http://<?=hostname();?>"> --> 
   <link rel="stylesheet" type="text/css" href="<?=hostname();?>evolution/formate.css"> 
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
   </head> 
@@ -85,8 +119,8 @@ $pass_ok = false;
 <table width="519"> 
 <tr> 
 <?php
-    if ( $pass_ok ) echo "   <th><font color=\"lime\">".va("Пароль отправлен на #1.", "xxx")."</font></th>\n";
-    else echo " <th><font color=\"red\">Постоянный адрес неверен.</font></th> \n";
+    if ( $pass_ok ) echo "   <th><font color=\"lime\">".va(loca("REG_FORGOT_OK"), $user['oname'])."</font></th>\n";
+    else echo " <th><font color=\"red\">".loca("REG_FORGOT_ERROR")."</font></th> \n";
 ?>
 </tr> 
 </table> 
--- trunk/game/reg/mail.php
+++ trunk/game/reg/mail.php
@@ -20,6 +20,10 @@ require_once "../user.php";
 require_once "../queue.php";
 require_once "../uni.php";
 require_once "../debug.php";
+require_once "../loca.php";
+
+$loca_lang = $_COOKIE['ogamelang'];
+loca_add ( "reg", $loca_lang );
 
 function method () { return $_SERVER['REQUEST_METHOD']; }
 
@@ -33,7 +37,7 @@ function hostname () {
 
 <html> 
 <head> 
-<title>Восстановление пароля Огейм</title> 
+<title><?=loca("REG_MAIL_TITLE");?></title> 
 <link rel="stylesheet" type="text/css" href="<?=hostname();?>evolution/formate.css"> 
   <link rel='stylesheet' type='text/css' href='/game/css/default.css' /> 
   <link rel='stylesheet' type='text/css' href='/game/css/formate.css' /> 
@@ -44,15 +48,15 @@ function hostname () {
 <div class="mybody"> 
 <form action="fa_pass.php" method="post"> 
 <div align="center"> 
-  <h2>Послать пароль</h2> 
-  Введите действительный в игре адрес.<table align="center"> 
+  <h2><?=loca("REG_MAIL_SEND");?></h2> 
+  <?=loca("REG_MAIL_NOTE");?><table align="center"> 
 <tr> 
-        <td>E-Mail:</td> 
+        <td><?=loca("REG_MAIL_EMAIL");?></td> 
         <td><input type="text" name="email"></td> 
 </tr> 
 <tr> 
         <td></td> 
-        <td><input type="submit" name="send_pass" value="Отправить данные"></td> 
+        <td><input type="submit" name="send_pass" value="<?=loca("REG_MAIL_SUBMIT");?>"></td> 
 </tr> 
 </table> 
 </form> 
