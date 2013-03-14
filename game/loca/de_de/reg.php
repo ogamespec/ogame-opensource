13f77d002ac1d0290df2e39dc8a7f702f533cc92
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2013-03-14 08:27:12 +0000

    - password recovery

---
+++ game/loca/de_de/reg.php
@@ -0,0 +1,23 @@
+<?php
+
+// Registerunden, loginen, email restorationichtenunden
+
+$LOCA["de"]["REG_MAIL_TITLE"] = "Übersicht";
+$LOCA["de"]["REG_MAIL_SEND"] = "Passwort senden";
+$LOCA["de"]["REG_MAIL_NOTE"] = "Bitte die permanente E-Mail Adresse des Spielaccounts eingeben.";
+$LOCA["de"]["REG_MAIL_EMAIL"] = "E-Mail:";
+$LOCA["de"]["REG_MAIL_SUBMIT"] = "Zugangsdaten Senden";
+
+$LOCA["de"]["REG_FORGOT_TITLE"] = "OGame Passwort versenden";
+$LOCA["de"]["REG_FORGOT_ERROR"] = "Keine gültige permanente Emailaddresse.";
+$LOCA["de"]["REG_FORGOT_OK"] = "Das Passwort wurde an #1 verschickt.";
+$LOCA["de"]["REG_FORGOT_SUBJ"] = "OGame Passwort";
+$LOCA["de"]["REG_FORGOT_MAIL"] = "Hallo #1,\n\n" .
+"dein Passwort für das OGame Universum #2 lautet:\n\n" .
+"#3\n\n" .
+"Du kannst dich damit unter #4 einloggen.\n\n" .
+"Wir verschicken Passwörter nur an die von dir im Spiel angegebenen Mailadressen. Solltest du diese Mail nicht angefordert haben kannst du sie also einfach ignorieren.\n\n" .
+"Wir wünschen dir weiterhin viel Erfolg beim Spielen von OGame!\n\n" .
+"Dein OGame-Team";
+
+?>
