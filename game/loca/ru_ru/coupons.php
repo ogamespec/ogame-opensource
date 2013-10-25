e3b10133a39d1c8b9913924e712daf95a9b5dfe4
ogamespec@gmail.com <ogamespec@gmail.com@9469c2ea-1eae-e622-0568-330b80004140>
2013-10-25 13:40:58 +0000

    - coupons almost done

--- game/loca/ru_ru/coupons.php
+++ game/loca/ru_ru/coupons.php
@@ -3,7 +3,16 @@
 // Языковые ключи, связанные с купонами.
 
 $LOCA["ru"]["COUPON_SUBJ"] = "Купон в подарок";
-$LOCA["ru"]["COUPON_MESSAGE"] = "Уважаемый #1 вам купон : #2";
+$LOCA["ru"]["COUPON_MESSAGE"] = "<html>
+<meta http-equiv='content-type' content='text/html; charset=UTF-8' />
+<body style='background-color:#415680; font-size: 15px; font-family: Tahoma,sans-serif; font-weight: bold; color: #E6EBFB;'>
+<p>Уважаемый #1!</p>
+<p>Прими в дар от проекта OGame Open Source этот купон:<p>
+<p><big style='font-size: 22px;'>#2</big></p>
+<p>Использовать его можно в Офицерском казино, прямо в игре. Нанимай командиров, вызывай скупщика и твоя империя будет процветать!</p>
+<p>Удачной игры!</p>
+</body>
+</html>";
 
 
 ?>
