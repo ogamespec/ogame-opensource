0c50c61f9e79a284cdc368005158078cae81057f
Andruxa <ogamespec@gmail.com>
2015-04-26 23:49:07 +0300

    hackmsg translate

---
+++ game/loca/de_de/hackmsg.php
@@ -0,0 +1,9 @@
+<?php
+
+// Hacker versuchen, Debug-Meldungen.
+// Dargestellt als Debug-Meldungen mit Hacker versuchen Note
+
+$LOCA["de"]["HACK_SELECT_PLANET"] = "Auswählen von fremden Planeten oder spezielle Galaxy Objekt.";
+$LOCA["de"]["HACK_SQL_INJECTION"] = "Mögliche SQL-Injektion Verletzung (spezifische Keywords in URI Anfrage gefunden oder GET / POST-Parameter).";
+
+?>
