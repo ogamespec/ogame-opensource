18a4cb9532fa7238c89c5a6ba347ee001a8b0ea0
org <ogamespec@gmail.com>
2024-01-21 23:18:20 +0300

    Вернул aa терм, на всякий случай. Его назначение не очень понятно, но пусть останется.

--- game/js/utilities.js
+++ game/js/utilities.js
@@ -1,11 +1,14 @@
 function t(){
+  v = new Date();
   n = new Date();
+  o = new Date();
   for (cn = 1; cn <= anz; cn++) {
     bxx = document.getElementById('bxx' + cn);
     if (!('dpp' in bxx)) {
       bxx.dpp = n.getTime() + bxx.title * 1000;
     }
-    s = Math.round((bxx.dpp - n.getTime()) / 1000.);
+    ss = Math.round((bxx.dpp - n.getTime()) / 1000.);
+    s = ss - Math.round((n.getTime() - v.getTime()) / 1000.);
     m = 0;
     h = 0;
     if (s < 0) {
