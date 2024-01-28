6abfbbabd899b32f278d37f2f1a9e02bea0e04ab
org <ogamespec@gmail.com>
2024-01-28 20:51:05 +0300

    Admin_RakSim done

--- game/battle.php
+++ game/battle.php
@@ -1175,84 +1175,127 @@ function ExpeditionBattle ( $fleet_id, $pirates, $level, $when )
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Ракетная атака.
-function RocketAttack ( $fleet_id, $planet_id, $when )
+// Алгоритмическая часть ракетной атаки (без работы с БД).
+function RocketAttackMain ( $amount, $primary, $moon_attack, &$target, &$moon_planet, $origin_user_attack, $target_user_armor )
 {
     global $UnitParam;
 
-    $fleet = LoadFleet ($fleet_id);
-    $amount = $fleet['ipm_amount'];
-    $primary = $fleet['ipm_target'];
-    $origin = GetPlanet ($fleet['start_planet']);
-    $target = GetPlanet ($planet_id);
-    $origin_user = LoadUser ($origin['owner_id']);
-    $target_user = LoadUser ($target['owner_id']);
-
     // Отбить атаку МПР перехватчиками
     $ipm = $amount;
-    $abm = $target['d502'];
+    $abm = $moon_attack ? $moon_planet['d502'] : $target['d502'];
     $ipm = max (0, $ipm - $abm);
     $ipm_destroyed = $amount - $ipm;
-    $target['d502'] -= $ipm_destroyed;
+    if ($moon_attack) $moon_planet['d502'] -= $ipm_destroyed;
+    else $target['d502'] -= $ipm_destroyed;
 
-    $maxdamage = $ipm * 12000 * (1 + $origin_user['r109'] / 10);
+    $maxdamage = $ipm * $UnitParam[503][2] * (1 + $origin_user_attack / 10);
 
     // Произвести атаку первичной цели
     if ( $primary > 0 && $ipm > 0 )
     {
-        $armor = $UnitParam[$primary][0] * (1 + 0.1 * $target_user['r111']) / 10;
+        $armor = $UnitParam[$primary][0] * (1 + 0.1 * $target_user_armor) / 10;
         $count = $target["d$primary"];
-        $destroyed = min ( floor ( $maxdamage / $armor ), $count );
-        $target["d$primary"] -= $destroyed;
-        $maxdamage -= $destroyed * $armor;
-        $maxdamage -= $destroyed;
+        if ($count != 0) {
+            $destroyed = min ( floor ( $maxdamage / $armor ), $count );
+            $target["d$primary"] -= $destroyed;
+            $maxdamage -= $destroyed * $armor;
+            $maxdamage -= $destroyed;
+        }
     }
 
-    // Расчитать потери обороны, если еще остались МПР
+    // Расчитать потери обороны, если еще остались МПР -- всё то же самое, но в ID не учитывать ID первичной цели. Чужие ракеты также можно бомбить.
     if ($maxdamage > 0)
     {
         $defmap = array ( 401, 402, 403, 404, 405, 406, 407, 408, 502, 503 );
         foreach ($defmap as $i=>$id)
         {
             if ($id == $primary) continue;
-            $armor = $UnitParam[$id][0] * (1 + 0.1 * $target_user['r111']) / 10;
+            $armor = $UnitParam[$id][0] * (1 + 0.1 * $target_user_armor) / 10;
             $count = $target["d$id"];
-            if ($count == 0) continue;
-            $destroyed = min ( floor ( $maxdamage / $armor ), $count );
-            $target["d$id"] -= $destroyed;
-            $maxdamage -= $destroyed * $armor;
-            $maxdamage -= $destroyed;
+            if ($count != 0) {
+                $destroyed = min ( floor ( $maxdamage / $armor ), $count );
+                $target["d$id"] -= $destroyed;
+                $maxdamage -= $destroyed * $armor;
+                $maxdamage -= $destroyed;
+            }
             if ($maxdamage <= 0) break;
         }
     }
 
+    return $ipm_destroyed;
+}
+
+// Ракетная атака.
+function RocketAttack ( $fleet_id, $planet_id, $when )
+{
+    $fleet = LoadFleet ($fleet_id);
+    $amount = $fleet['ipm_amount'];
+    $primary = $fleet['ipm_target'];
+    $origin = GetPlanet ($fleet['start_planet']);
+    $target = GetPlanet ($planet_id);
+    $moon_attack = $target['type'] == 0;
+    if ($moon_attack) {
+        // Если ракетная атака производится на Луну, то перехватчики с планеты участвуют в защите
+        $moon_planet = LoadPlanet ($target['g'], $target['s'], $target['p'], 1);
+    }
+    $origin_user = LoadUser ($origin['owner_id']);
+    $target_user = LoadUser ($target['owner_id']);
+
+    $ipm_destroyed = RocketAttackMain (
+        $amount, 
+        $primary, 
+        $moon_attack, 
+        $target, 
+        $moon_planet, 
+        $origin_user['r109'], 
+        $target_user['r111'] );
+
     // Записать назад потери обороны.
     SetPlanetDefense ( $planet_id, $target );
+    if ($moon_attack) {
+        SetPlanetDefense ( $moon_planet['planet_id'], $moon_planet );
+    }
 
     // Изменить статистику игроков
     RecalcRanks ();
 
-    $text = "$amount ракетам из общего числа выпущенных ракет с планеты ".$origin['name']." <a href=# onclick=showGalaxy(".$origin['g'].",".$origin['s'].",".$origin['p']."); >[".$origin['g'].":".$origin['s'].":".$origin['p']."]</a>  ";
-    $text .= "удалось попасть на Вашу планету ".$target['name']." <a href=# onclick=showGalaxy(".$target['g'].",".$target['s'].",".$target['p']."); >[".$target['g'].":".$target['s'].":".$target['p']."]</a> !<br>";
-    if ($ipm_destroyed) $text .= "$ipm_destroyed ракет(-ы) было уничтожено Вашими ракетами-перехватчиками<br>:<br>";
-
-    $defmap = array ( 503, 502, 408, 407, 406, 405, 404, 403, 402, 401 );
-    $text .= "<table width=400><tr><td class=c colspan=4>Поражённая оборона</td></tr>";
-    $n = 0;
-    foreach ( $defmap as $i=>$gid )
-    {
-        if ( ($n % 2) == 0 ) $text .= "</tr>";
-        if ( $target["d$gid"] ) {
-            $text .= "<td>".loca("NAME_$gid")."</td><td>".nicenum($target["d$gid"])."</td>";
-            $n++;
-        }
-    }
-    $text .= "</table><br>\n";
-
     // Обновить активность на планете.
     UpdatePlanetActivity ( $planet_id, $when );
 
+    // Часть сообщения с повреждениями обороны
+    $defmap = array ( 503, 502, 408, 407, 406, 405, 404, 403, 402, 401 );
+    $deftext = "<table width=400><tr><td class=c colspan=4>Поражённая оборона</td></tr>";
+    $n = 0;
+    foreach ( $defmap as $i=>$gid )
+    {
+        if ( ($n % 2) == 0 ) $deftext .= "</tr>";
+        if ( $target["d$gid"] ) {
+
+            $count = $target["d$gid"];
+            // Учесть оборону луны перехватчиками с планеты
+            if ($moon_attack && $gid == 502 ) {
+                $count = $moon_planet["d502"];
+            }
+
+            $deftext .= "<td>".loca("NAME_$gid")."</td><td>".nicenum($count)."</td>";
+            $n++;
+        }
+    }
+    $deftext .= "</table><br>\n";
+
+    // Сформировать сообщение для защитника
+    $text = "$amount ракетам из общего числа выпущенных ракет с планеты ".$origin['name']." <a href=# onclick=showGalaxy(".$origin['g'].",".$origin['s'].",".$origin['p']."); >[".$origin['g'].":".$origin['s'].":".$origin['p']."]</a>  ";
+    $text .= "удалось попасть на Вашу планету ".$target['name']." <a href=# onclick=showGalaxy(".$target['g'].",".$target['s'].",".$target['p']."); >[".$target['g'].":".$target['s'].":".$target['p']."]</a> !<br>";
+    if ($ipm_destroyed) $text .= "$ipm_destroyed ракет(-ы) было уничтожено Вашими ракетами-перехватчиками<br>:<br>";
+    $text .= $deftext;
     SendMessage ( $target_user['player_id'], "Командование флотом", "Ракетная атака", $text, 2, $when);
+
+    // Сформировать сообщение для атакующего: https://github.com/ogamespec/ogame-opensource/issues/61
+    // Оригинальная версия 0.84 не создавала сообщения для атакующего.
+    $text = "$amount ракет(ы) с Вашей планеты ".$origin['name']." <a href=# onclick=showGalaxy(".$origin['g'].",".$origin['s'].",".$origin['p']."); >[".$origin['g'].":".$origin['s'].":".$origin['p']."]</a> ";
+    $text .= "ударили по планете ".$target['name']." <a href=# onclick=showGalaxy(".$target['g'].",".$target['s'].",".$target['p']."); >[".$target['g'].":".$target['s'].":".$target['p']."]</a> !<br>";    
+    $text .= $deftext;
+    SendMessage ( $origin_user['player_id'], "Командование флотом", "Ракетная атака", $text, 2, $when);
 }
 
 ?>
--- game/pages/admin_raksim.php
+++ game/pages/admin_raksim.php
@@ -1,22 +1,138 @@
 <?php
 
-// Админка: симулятор ракетной атаки
+// Админка: симулятор ракетной атаки. Используется для верификации и отладки алгоритмической части ракетной атаки.
 
 function Admin_RakSim ()
 {
     global $session;
     global $db_prefix;
     global $GlobalUser;
+    global $GlobalUni;
+
+    $defmap = array ( 401, 402, 403, 404, 405, 406, 407, 408, 502, 503 );
+    $def = array ();
+
+    foreach ($defmap as $i=>$gid) {
+        $def[$gid] = 0;
+    }
+
+    loca_add ( "galaxy", $GlobalUni['lang'] );
+
+    $a_weap = 0;
+    $d_armor = 0;
+
+    $anz = 0;
+    $pziel = 0;
 
     // Обработка POST-запроса.
-    if ( method () === "POST" )
-    {
+    if ( method () === "POST" && $GlobalUser['admin'] != 0 ) {
+        //print_r ( $_POST );
+        //echo "<hr>";
+
+        if ( key_exists ( 'a_weap', $_POST) ) $a_weap = intval ($_POST['a_weap']);
+        if ( key_exists ( 'd_armor', $_POST) ) $d_armor = intval ($_POST['d_armor']);
+
+        if ( key_exists ( 'anz', $_POST) ) $anz = intval ($_POST['anz']);
+        if ( key_exists ( 'pziel', $_POST) ) $pziel = intval ($_POST['pziel']);
+
+        $target = array();
+        $moon_planet = array();     // не используется
+
+        foreach ($defmap as $i=>$gid) {
+            if ( key_exists ( 'd_'.$gid, $_POST) ) {
+                $def[$gid] = intval ($_POST['d_'.$gid]);
+            }
+            $target["d".$gid] = $def[$gid];
+        }
+
+        $ipm_destroyed = RocketAttackMain (
+            $anz,
+            $pziel,
+            false,
+            $target,
+            $moon_planet,
+            $a_weap,
+            $d_armor );
+
+        foreach ($defmap as $i=>$gid) {
+            $def[$gid] = $target["d".$gid];
+        }
     }
 ?>
 
 <?=AdminPanel();?>
 
-TODO
+<table cellpadding=0 cellspacing=0>
+<form name="simForm" action="index.php?page=admin&session=<?=$session;?>&mode=RakSim" method="POST" >
+
+<tr>        <td class=c>Атакующий</td>                <td class=c>Обороняющийся</td>  </tr>
+
+<tr> 
+<td> 
+    Вооружение: <input type="text" name="a_weap" size=2 value="<?=$a_weap;?>"> 
+<td> 
+    Броня: <input type="text" name="d_armor" size=2 value="<?=$d_armor;?>"></td> 
+</tr>
+
+
+        <tr> <th valign=top>
+        <table>
+
+<tr><td colspan=2> 
+<table>
+<tr><td class=c colspan=2>Настройки</td></tr>
+
+<tr><td>
+<?=loca("NAME_502");?>:     <input type="text" name="anz" size="2" maxlength="2" value="<?=$anz;?>"/></td></tr>
+
+    <tr><td>
+    <?=loca("GALAXY_RAK_TARGET");?>:
+     <select name="pziel">
+      <option value="0" <?php if($pziel == 0) echo "selected";?> ><?=loca("GALAXY_RAK_TARGET_ALL");?></option>
+<?php
+    foreach ($defmap as $i=>$gid)
+    {
+        if ($gid > 500) {
+            // Не нужно учитывать ракетную оборону.
+            break;
+        }
+        echo "       <option value=\"$gid\" ";
+        if($pziel == $gid) echo "selected";
+        echo ">".loca("NAME_$gid")."</option>\n";
+    }
+?>
+           </select>
+    </td></tr>
+
+</table>
+</td></tr>
+
+        </table>
+        </th>
+
+
+
+        <th valign=top>
+        <table>
+
+<?php
+
+    echo "<tr><td class=c colspan=2><b>Оборона</b></td></tr>\n";
+    $defmap = array ( 401, 402, 403, 404, 405, 406, 407, 408, 502, 503 );
+    foreach ($defmap as $i=>$gid)
+    {
+?>
+           <tr><td> <?=loca("NAME_$gid");?> </td> <td> <input name="d_<?=$gid;?>" size=5 value=<?=$def[$gid];?>> </td> </tr>
+<?php
+    }
+?>
+        </table>
+        </th></tr>            
+
+
+<tr><td colspan=2><center><input type="submit" value="Ракетная атака"></center></td></tr>
+</form>
+</table>
 
 <?php
 } 		// Admin_RakSim
