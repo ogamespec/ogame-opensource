46411eaeea749cef927a60466a40c2705be3f4d1
Tsamsiyu1 <32683118+Tsamsiyu1@users.noreply.github.com>
2024-02-12 10:29:44 +0100

    Update admin.php

---
+++ game/loca/de_de/admin.php
@@ -0,0 +1,27 @@
+<?php
+
+// Admin tool.
+
+$LOCA["de"]["ADM_MENU_FLEETLOGS"] = "Flottenprotokoll";
+$LOCA["de"]["ADM_MENU_BROWSE"] = "Verlauf durchsuchen";
+$LOCA["de"]["ADM_MENU_REPORTS"] = "Berichte";
+$LOCA["de"]["ADM_MENU_BANS"] = "Bans";
+$LOCA["de"]["ADM_MENU_USERS"] = "Benutzer";
+$LOCA["de"]["ADM_MENU_PLANETS"] = "Planeten";
+$LOCA["de"]["ADM_MENU_QUEUE"] = "Warteschlange";
+$LOCA["de"]["ADM_MENU_UNI"] = "Universum Einstellungen";
+$LOCA["de"]["ADM_MENU_ERRORS"] = "Fehler";
+$LOCA["de"]["ADM_MENU_DEBUG"] = "Fehlersuche";
+$LOCA["de"]["ADM_MENU_BATTLESIM"] = "Kampfsimulator";
+$LOCA["de"]["ADM_MENU_BROADCAST"] = "Benachrichtigung";
+$LOCA["de"]["ADM_MENU_EXPEDITION"] = "Expeditionseinstellungen";
+$LOCA["de"]["ADM_MENU_LOGINS"] = "Anmeldungen";
+$LOCA["de"]["ADM_MENU_CHECKSUM"] = "Quellenprüfung";
+$LOCA["de"]["ADM_MENU_BOTS"] = "Bot-Kontrollen";
+$LOCA["de"]["ADM_MENU_BATTLELOGS"] = "Kampfberichte";
+$LOCA["de"]["ADM_MENU_USERLOGS"] = "Benutzerprotokolle";
+$LOCA["de"]["ADM_MENU_BOTEDIT"] = "Bot Editor";
+$LOCA["de"]["ADM_MENU_COUPONS"] = "Gutscheine";
+$LOCA["de"]["ADM_MENU_RAKSIM"] = "Raketenangriffssimulation";
+
+?>
