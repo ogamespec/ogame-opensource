a0f75c0c776ef37c76349647011aa652783f5ea9
org <ogamespec@gmail.com>
2024-02-13 11:04:17 +0300

    Ally ranks page loca

--- game/pages/allianzen_ranks.php
+++ game/pages/allianzen_ranks.php
@@ -14,7 +14,7 @@ function PageAlly_Ranks ()
     $myrank = LoadRank ( $ally['ally_id'], $GlobalUser['allyrank'] );
     if ( ! ($myrank['rights'] & 0x020) )
     {
-        $AllianzenError = "<center>\nНедостаточно прав для проведения операции<br></center>";
+        $AllianzenError = "<center>\n".loca("ALLY_NO_WAY")."<br></center>";
         return;
     }
 
@@ -22,7 +22,7 @@ function PageAlly_Ranks ()
     {
         if ( key_exists ('newrangname', $_POST) )       // создать ранг
         {
-            if ( !preg_match ("/^[a-zA-Z0-9\.\_\- ]+$/", $_POST['newrangname'] ) ) $AllianzenError = "<center>\nРанг содержит особые символы<br></center>";
+            if ( !preg_match ("/^[a-zA-Z0-9\.\_\- ]+$/", $_POST['newrangname'] ) ) $AllianzenError = "<center>\n".loca("ALLY_RANK_ERROR_SPECIAL_CHARS")."<br></center>";
             else AddRank ( $ally['ally_id'], $_POST['newrangname'] );
         }
         else                                                              // изменить ранги
@@ -36,7 +36,8 @@ function PageAlly_Ranks ()
                 $mask = $rank['rights'];
                 for ($i=0; $i<9; $i++)
                 {
-                    if ( $_POST["u".$rank['rank_id']."r$i"] === "on" ) $mask |= (1 << $i);
+                    $mask_name = "u".$rank['rank_id']."r$i";
+                    if ( key_exists($mask_name, $_POST) && $_POST[$mask_name] === "on" ) $mask |= (1 << $i);
                     else $mask &= ~(1 << $i);
                 }
                 SetRank ( $ally['ally_id'], $rank['rank_id'], $mask );
@@ -55,15 +56,15 @@ function PageAlly_Ranks ()
 
 ?>
 <script src="js/cntchar.js" type="text/javascript"></script><script src="js/win.js" type="text/javascript"></script><br />
-<a href="index.php?page=allianzen&session=<?=$session;?>&a=5">Назад к обзору</a>
+<a href="index.php?page=allianzen&session=<?=$session;?>&a=5"><?=loca("ALLY_BACK");?></a>
 <table width="519">
  <tr>
-  <td class="c" colspan="11">Сформировать права</td>
+  <td class="c" colspan="11"><?=loca("ALLY_RANK_MASK");?></td>
  </tr>
  <form action="index.php?page=allianzen&session=<?=$session;?>&a=15" method="POST">
  <tr>
   <th></th>
-  <th>Название ранга</th>
+  <th><?=loca("ALLY_RANK_NAME");?></th>
   <th>
    <img src=img/r1.png>
   </th>
@@ -101,7 +102,7 @@ function PageAlly_Ranks ()
         $rank = dbarray ($result);
         if ( $rank['rank_id'] == 0 || $rank['rank_id'] == 1 ) continue;    // Основателя и Новичка не показываем.
         echo " <tr>\n";
-        echo "  <th><a href=\"index.php?page=allianzen&session=$session&a=15&d=".$rank['rank_id']."\"><img src=\"".UserSkin()."pic/abort.gif\" alt=\"Удалить ранг\" border=\"0\"></a></th>\n";
+        echo "  <th><a href=\"index.php?page=allianzen&session=$session&a=15&d=".$rank['rank_id']."\"><img src=\"".UserSkin()."pic/abort.gif\" alt=\"".loca("ALLY_RANK_DELETE")."\" border=\"0\"></a></th>\n";
         echo "  <th>&nbsp;".$rank['name']."&nbsp;</th>\n";
         for ($r=0; $r<9; $r++) {
             if ($rank['rights'] & (1 << $r))  echo "<th><input type=checkbox name=\"u".$rank['rank_id']."r$r\" checked></th>";
@@ -111,29 +112,29 @@ function PageAlly_Ranks ()
     }
 ?>
  <tr>
-  <th colspan="11"><input type="submit" value="Сохранить"></th>
+  <th colspan="11"><input type="submit" value="<?=loca("ALLY_RANK_SAVE");?>"></th>
  </tr>
 </form>
 </table>
 <br /><form action="index.php?page=allianzen&session=<?=$session;?>&a=15" method=POST>
 <table width=519>
-<tr><td class=c colspan=2>Назначить новый ранг</td></tr>
-<tr><th>Название ранга</th><th><input type=text name="newrangname" size=20 maxlength=30></th></tr>
-<tr><th colspan=2><input type=submit value="Назначить"></th></tr>
+<tr><td class=c colspan=2><?=loca("ALLY_RANK_ADD_TEXT");?></td></tr>
+<tr><th><?=loca("ALLY_RANK_NAME");?></th><th><input type=text name="newrangname" size=20 maxlength=30></th></tr>
+<tr><th colspan=2><input type=submit value="<?=loca("ALLY_RANK_ADD_SUBMIT");?>"></th></tr>
 </form></table>
 
 <br/><form action="index.php?page=allianzen&session=<?=$session;?>&a=15" method=POST>
 <table width=519>
-<tr><td class=c colspan=2>Пояснение прав</td></tr>
-<tr><th><img src=img/r1.png></th><th>Распустить альянс</th></tr>
-<tr><th><img src=img/r2.png></th><th>Выгнать игрока</th></tr>
-<tr><th><img src=img/r3.png></th><th>Посмотреть заявления</th></tr>
-<tr><th><img src=img/r4.png></th><th>Посмотреть список членов</th></tr>
-<tr><th><img src=img/r5.png></th><th>Редактировать заявления</th></tr>
-<tr><th><img src=img/r6.png></th><th>Управление альянсом</th></tr>
-<tr><th><img src=img/r7.png></th><th>Посмотреть статус "он-лайн" в списке членов</th></tr>
-<tr><th><img src=img/r8.png></th><th>Составить общее сообщение</th></tr>
-<tr><th><img src=img/r9.png></th><th>'Правая рука' (необходимо для передачи статуса основателя)</th></tr>
+<tr><td class=c colspan=2><?=loca("ALLY_RANK_INFO");?></td></tr>
+<tr><th><img src=img/r1.png></th><th><?=loca("ALLY_RANK_1");?></th></tr>
+<tr><th><img src=img/r2.png></th><th><?=loca("ALLY_RANK_2");?></th></tr>
+<tr><th><img src=img/r3.png></th><th><?=loca("ALLY_RANK_3");?></th></tr>
+<tr><th><img src=img/r4.png></th><th><?=loca("ALLY_RANK_4");?></th></tr>
+<tr><th><img src=img/r5.png></th><th><?=loca("ALLY_RANK_5");?></th></tr>
+<tr><th><img src=img/r6.png></th><th><?=loca("ALLY_RANK_6");?></th></tr>
+<tr><th><img src=img/r7.png></th><th><?=loca("ALLY_RANK_7");?></th></tr>
+<tr><th><img src=img/r8.png></th><th><?=loca("ALLY_RANK_8");?></th></tr>
+<tr><th><img src=img/r9.png></th><th><?=loca("ALLY_RANK_9");?></th></tr>
 </form></table>
 <?php
 }
