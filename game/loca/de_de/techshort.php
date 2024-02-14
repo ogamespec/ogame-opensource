6f63f6582b0caa51ef13f89370d0dc802873354d
org <ogamespec@gmail.com>
2024-02-14 22:34:01 +0300

    techshort De completed

--- game/loca/de_de/techshort.php
+++ game/loca/de_de/techshort.php
@@ -1,68 +1,67 @@
 <?php
 
-// TODO: Kurzbeschreibung
-// Es gibt nur wenige HTML-Quellen für deutsche Seiten..
+// Kurzbeschreibung
 
 $LOCA["de"]["SHORT_1"] = "Hauptrohstofflieferanten fur den Bau tragender Strukturen von Bauwerken und Schiffen.";
 $LOCA["de"]["SHORT_2"] = "Hauptrohstofflieferanten fur elektronische Bauteile und Legierungen.";
 $LOCA["de"]["SHORT_3"] = "Entziehen dem Wasser eines Planeten den geringen Deuteriumanteil.";
 $LOCA["de"]["SHORT_4"] = "Solarkraftwerke gewinnen Energie aus Sonneneinstrahlung. Einige Gebaude benotigen Energie fur ihren Betrieb.";
-//$LOCA["de"]["SHORT_12"] = "Добывает энергию из процесса образования атома гелия двумя атомами тяжёлого водорода.";
+$LOCA["de"]["SHORT_12"] = "Das Fusionskraftwerk gewinnt Energie aus der Fusion von 2 schweren Wasserstoffatomen zu einem Heliumatom.";
 $LOCA["de"]["SHORT_14"] = "Roboterfabriken stellen einfache Arbeitskrafte zur Verfugung, die beim Bau der planetaren Infrastruktur eingesetzt werden konnen. Jede Stufe erhoht damit die Geschwindigkeit des Ausbaus von Gebauden.";
-//$LOCA["de"]["SHORT_15"] = "Представляет собой венец робототехники. Каждый уровень укорачивает время строительства зданий, кораблей и оборонительных сооружений вдвое.";
-//$LOCA["de"]["SHORT_21"] = "В строительной верфи производятся все виды кораблей и оборонительных сооружений.";
-//$LOCA["de"]["SHORT_22"] = "Хранилище для необработанных руд металлов до их дальнейшей переработки.";
-//$LOCA["de"]["SHORT_23"] = "Хранилище для необработанного кристалла до его дальнейшей переработки.";
-//$LOCA["de"]["SHORT_24"] = "Огромные ёмкости для хранения добытого дейтерия.";
+$LOCA["de"]["SHORT_15"] = "Dies ist die Krönung der Robotertechnik. Jede Stufe halbiert die Bauzeit von Gebäuden, Schiffen und Verteidigung.";
+$LOCA["de"]["SHORT_21"] = "In der planetaren Werft werden alle Arten von Schiffen und Verteidigungsanlagen gebaut.";
+$LOCA["de"]["SHORT_22"] = "Lagerstätte für rohe Metallerze, bevor sie weiter verarbeitet werden.";
+$LOCA["de"]["SHORT_23"] = "Lagerstätte für rohe Kristalle, bevor sie weiterverarbeitet werden.";
+$LOCA["de"]["SHORT_24"] = "Riesige Tanks zur Lagerung des neu gewonnenen Deuteriums.";
 $LOCA["de"]["SHORT_31"] = "Um neue Technologien zu erforschen, ist der Betrieb einer Forschungsstation notwendig.";
-//$LOCA["de"]["SHORT_33"] = "Терраформер увеличивает пригодную для застройки площадь";
-//$LOCA["de"]["SHORT_34"] = "Склад альянса предоставляет возможность обеспечения топливом дружественных флотов, которые помогают при обороне и находятся на орбите";
-//$LOCA["de"]["SHORT_41"] = "Луна не располагает атмосферой, поэтому перед заселением требуется соорудить лунную базу.";
-//$LOCA["de"]["SHORT_42"] = "Позволяет наблюдать за передвижениями флота. Чем выше уровень развития, тем больше радиус действия фаланги.";
-//$LOCA["de"]["SHORT_43"] = "Ворота - это огромные телепортеры, которые могут пересылать между собой флоты любых размеров без временных затрат.";
-//$LOCA["de"]["SHORT_44"] = "Служит для хранения ракет.";
+$LOCA["de"]["SHORT_33"] = "Terraformer vergrößern die nutzbare Fläche auf einem Planeten.";
+$LOCA["de"]["SHORT_34"] = "Das Allianzdepot bietet dir die Möglichkeit, befreundete Flotten, die bei der Verteidigung helfen und in deinem Orbit stehen, mit Treibstoff zu versorgen.";
+$LOCA["de"]["SHORT_41"] = "Ein Mond verfügt über keinerlei Atmosphäre, deshalb muss vor der Besiedlung eine Mondbasis errichtet werden.";
+$LOCA["de"]["SHORT_42"] = "Mithilfe der Sensorphalanx können Flottenbewegungen beobachtet werden. Je höher die Ausbaustufe, desto größer die Reichweite der Phalanx.";
+$LOCA["de"]["SHORT_43"] = "Sprungtore sind riesige Transmitter, die selbst große Flotten ohne Zeitverlust durch das Universum schicken können.";
+$LOCA["de"]["SHORT_44"] = "Raketensilos dienen zum Einlagern von Raketen.";
 
-//$LOCA["de"]["SHORT_106"] = "С помощью этой технологии добываются данные о других планетах.";
-//$LOCA["de"]["SHORT_108"] = "С увеличением мощности компьютеров можно командовать всё большим количеством флотов. Каждый уровень компьютерной технологии увеличивает максимальное количество флотов на один.";
-//$LOCA["de"]["SHORT_109"] = "Оружейная технология делает системы вооружения эффективней. Каждый уровень увеличивает мощность вооружения войсковых частей на 10% от основной мощности.";
-//$LOCA["de"]["SHORT_110"] = "Эта технология делает щиты кораблей и оборонительных сооружений эффективней. Каждый уровень повышает эффективность щитов на 10% от основной мощности.";
-//$LOCA["de"]["SHORT_111"] = "Специальные сплавы улучшают броню космических кораблей. Так, устойчивость брони может увеличиваться с каждым уровнем на 10% .";
-//$LOCA["de"]["SHORT_113"] = "Обладание различными видами энергии необходимо для многих новых технологий.";
-//$LOCA["de"]["SHORT_114"] = "Путём сплетения 4-го и 5-го измерения стало возможным исследовать новый более экономный и эффективный двигатель.";
-//$LOCA["de"]["SHORT_115"] = "Дальнейшее развитие этих двигателей делает некоторые корабли быстрее, однако каждый уровень повышает скорость лишь на 10%.";
-//$LOCA["de"]["SHORT_117"] = "Импульсный двигатель основывается на принципе отдачи. Дальнейшее развитие этих двигателей делает некоторые корабли быстрее, однако с каждым уровнем повышает скорость лишь на 20%.";
-//$LOCA["de"]["SHORT_118"] = "Выгибает пространство вокруг корабля. Дальнейшее развитие этих двигателей делает некоторые корабли быстрее, каждый уровень повышает скорость на 30%.";
-//$LOCA["de"]["SHORT_120"] = "Благодаря фокусированию света возникает луч, который при попадании на объект наносит ему повреждения.";
-//$LOCA["de"]["SHORT_121"] = "Поистине смертоносный наводимый луч из ускоренных ионов. При попадании на какой-либо объект они наносят огромный ущерб.";
-//$LOCA["de"]["SHORT_122"] = "Дальнейшее развитие ионной технологии, которая ускоряет не ионы, а высокоэнергетическую плазму. Она оказывает опустошительное действие при попадании на какой-либо объект.";
-//$LOCA["de"]["SHORT_123"] = "Посредством этой сети учёные с различных планет могут обмениваться информацией.";
-//$LOCA["de"]["SHORT_124"] = "Теперь корабли можно оснащать исследовательским модулем, обеспечивающим обработку собранных данных  в условиях длительных полётов.";
-//$LOCA["de"]["SHORT_199"] = "Путём запуска концентрированного заряда частиц гравитона можно создавать искусственное гравитационное поле, благодаря которому можно уничтожать корабли или даже луны.";
+$LOCA["de"]["SHORT_106"] = "Mithilfe dieser Technik lassen sich Informationen über andere Planeten und Monde gewinnen.";
+$LOCA["de"]["SHORT_108"] = "Mit der Erhöhung der Computerkapazitäten lassen sich immer mehr Flotten befehligen. Jede Stufe Computertechnik erhöht dabei die maximale Flottenanzahl um eins.";
+$LOCA["de"]["SHORT_109"] = "Die Waffentechnik steigert die Leistung sämtlicher Waffensysteme, so dass die Schusskraft jeder Einheit pro Stufe auf 10% über den Grundwert ansteigt.";
+$LOCA["de"]["SHORT_110"] = "Schildtechnik macht die Schilde der Schiffe und Verteidigungsanlagen effizienter. Jede Stufe steigert die Effizienz um 10% des Grundwertes.";
+$LOCA["de"]["SHORT_111"] = "Spezielle Legierungen verbessern die Panzerung der Raumschiffe stetig. Die Wirksamkeit der Panzerung kann so pro Stufe um 10% gesteigert werden.";
+$LOCA["de"]["SHORT_113"] = "Die Beherrschung der unterschiedlichen Arten von Energie ist für viele neue Technologien notwendig.";
+$LOCA["de"]["SHORT_114"] = "Durch die Einbindung der vierten und fünften Dimension ist es nun möglich, einen neuartigen Antrieb zu erforschen, der sparsamer und leistungsfähiger ist.";
+$LOCA["de"]["SHORT_115"] = "Die Weiterentwicklung dieser Triebwerke macht einige Schiffe schneller. Jede Stufe steigert die Geschwindigkeit um 10% des Grundwertes.";
+$LOCA["de"]["SHORT_117"] = "Das Impulstriebwerk basiert auf dem Rückstoßprinzip. Die Weiterentwicklung dieser Triebwerke steigert die Geschwindigkeit einiger Schiffe um 20% des Grundwertes.";
+$LOCA["de"]["SHORT_118"] = "Die Hyperraumtechnologie ist die gezielte Krümmung des Raums durch die Einbindung der vierten und fünften Dimension. Jede Stufe dieser Technologie beschleunigt deine Schiffe um 30% des Grundwertes.";
+$LOCA["de"]["SHORT_120"] = "Durch Bündelung des Lichts entsteht ein Strahl, der beim Auftreffen auf ein Objekt Schaden anrichtet.";
+$LOCA["de"]["SHORT_121"] = "Durch die Konzentration von Ionen können Geschütze gebaut werden, die beträchtlichen Schaden anrichten können.";
+$LOCA["de"]["SHORT_122"] = "Eine Weiterentwicklung der Ionentechnik, die hochenergetisches Plasma beschleunigt, das verheerende Schäden anrichten kann.";
+$LOCA["de"]["SHORT_123"] = "Forscher verschiedener Planeten kommunizieren über dieses Netzwerk miteinander.";
+$LOCA["de"]["SHORT_124"] = "Schiffe können nun mit einem Forschungsmodul ausgestattet werden, um gesammelte Daten unter Langzeitflugbedingungen zu verarbeiten.";   // TODO: Find original text
+$LOCA["de"]["SHORT_199"] = "Durch den Abschuss einer konzentrierten Ladung von Gravitonpartikeln kann ein künstliches Gravitationsfeld errichtet werden. Es hat das Potenzial, sogar große Schiffe oder ganze Monde zu vernichten.";
 
-//$LOCA["de"]["SHORT_202"] = "Малый транспорт - это маневренный корабль, который может быстро транспортировать сырье на другие планеты.";
-//$LOCA["de"]["SHORT_203"] = "Дальнейшее развитие малых транспортов позволило создать корабли, обладающие большей вместительностью и, благодаря более развитому двигателю, способными передвигаться быстрее, чем малый транспорт, до тех пор, пока на малых транспортах не устанавливаются импульсные двигатели";
+$LOCA["de"]["SHORT_202"] = "Der kleine Transporter ist ein wendiges Schiff, welches Rohstoffe schnell zu anderen Planeten transportieren kann.";
+$LOCA["de"]["SHORT_203"] = "Die Weiterentwicklung des kleinen Transporters hat ein größeres Ladevermögen und ist dank seines verbesserten Antriebs auch schneller.";
 $LOCA["de"]["SHORT_204"] = "Der leichte Jäger ist ein wendiges Schiff, das auf fast jedem Planeten vorgefunden wird. Die Kosten sind nicht besonders hoch, Schildstärke und Ladekapazität sind allerdings sehr gering.";
-//$LOCA["de"]["SHORT_205"] = "Дальнейшее развитие лёгкого истребителя, он лучше защищён и обладает большей силой атаки.";
-//$LOCA["de"]["SHORT_206"] = "Крейсеры почти втрое сильней защищены, чем тяжёлые истребители, а по огневой мощи они превосходят тяжёлые истребители более, чем в два раза. К тому же они очень быстры.";
-//$LOCA["de"]["SHORT_207"] = "Линкоры как правило составляют основу флота. Их тяжёлые орудия, высокая скорость и большой грузовой тоннаж делают их серьёзными противниками.";
-//$LOCA["de"]["SHORT_208"] = "Этот корабль может колонизировать свободные планеты.";
-//$LOCA["de"]["SHORT_209"] = "С помощью переработчика добывается сырьё из обломков.";
-//$LOCA["de"]["SHORT_210"] = "Шпионские зонды - это маленькие манёвренные корабли, которые доставляют с больших расстояний данные о флотах и планетах.";
-//$LOCA["de"]["SHORT_211"] = "Бомбардировщик был разработан специально для того, чтобы уничтожать планетарную защиту.";
+$LOCA["de"]["SHORT_205"] = "Diese Weiterentwicklung des leichten Jägers ist besser gepanzert und hat eine höhere Angriffsstärke.";
+$LOCA["de"]["SHORT_206"] = "Kreuzer sind fast dreimal so stark gepanzert wie schwere Jäger und verfügen über mehr als die doppelte Schusskraft. Zudem sind sie sehr schnell.";
+$LOCA["de"]["SHORT_207"] = "Schlachtschiffe sind das Rückgrat einer Flotte. Ihre schweren Geschütze und die hohe Geschwindigkeit machen sie zu ernst zu nehmenden Gegnern.";
+$LOCA["de"]["SHORT_208"] = "Mit diesem Schiff können fremde Planeten kolonisiert werden.";
+$LOCA["de"]["SHORT_209"] = "Mit dem Recycler lassen sich Rohstoffe aus Trümmerfeldern gewinnen.";
+$LOCA["de"]["SHORT_210"] = "Spionagesonden sind kleine wendige Drohnen, die über weite Entfernungen Daten zu Flotten und Planeten liefern.";
+$LOCA["de"]["SHORT_211"] = "Der Bomber wurde speziell entwickelt, um die Verteidigung eines Planeten zu zerstören.";
 $LOCA["de"]["SHORT_212"] = "Solarsatelliten sind einfache Plattformen aus Solarzellen, die sich in einem hohen stationären Orbit befinden. Sie sammeln das Sonnenlicht und geben es per Laser an die Bodenstation weiter.";
-//$LOCA["de"]["SHORT_213"] = "Уничтожитель - король среди военных кораблей.";
-//$LOCA["de"]["SHORT_214"] = "Уничтожительная мощь звезды смерти непревосходима.";
-//$LOCA["de"]["SHORT_215"] = "Линейный крейсер специализируется на перехвате вражеских флотов.";
+$LOCA["de"]["SHORT_213"] = "Der Zerstörer ist der König unter den Kriegsschiffen.";
+$LOCA["de"]["SHORT_214"] = "Die Zerstörungskraft des Todessterns ist unübertroffen.";
+$LOCA["de"]["SHORT_215"] = "Der Schlachtkreuzer ist auf das Abfangen feindlicher Flotten spezialisiert.";
 
-//$LOCA["de"]["SHORT_401"] = "Ракетная установка - простое и дешёвое средство обороны.";
-//$LOCA["de"]["SHORT_402"] = "При помощи концентрированного обстрела цели фотонами можно достичь значительно больших разрушений, чем при применении обычного баллистического вооружения.";
-//$LOCA["de"]["SHORT_403"] = "Тяжелый лазер представляет собой дальнейшее развитие лёгкого лазера.";
-//$LOCA["de"]["SHORT_404"] = "Пушка Гаусса ускоряет многотонные заряды с гигантскими затратами энергии.";
-//$LOCA["de"]["SHORT_405"] = "Ионное орудие направляет на цель волну ионов, которая дестабилизирует щиты и повреждает электронику.";
-//$LOCA["de"]["SHORT_406"] = "Плазменные орудия выпускают сгустки высокотемпературной плазмы и превосходят по своему разрушительному действию даже уничтожитель.";
-//$LOCA["de"]["SHORT_407"] = "Малый щитовой купол защищает планету и поглощает удары атаки.";
-//$LOCA["de"]["SHORT_408"] = "Дальнейшее развитие малого щитового купола. Он может сдерживать ещё более сильные атаки на планету, поглощая значительно большее количество энергии.";
-//$LOCA["de"]["SHORT_502"] = "Ракеты-перехватчики уничтожают атакующие межпланетные ракеты";
-//$LOCA["de"]["SHORT_503"] = "Межпланетные ракеты уничтожают защиту противника.";
+$LOCA["de"]["SHORT_401"] = "Der Raketenwerfer ist eine einfache und kostengünstige Verteidigungsmöglichkeit.";
+$LOCA["de"]["SHORT_402"] = "Durch den konzentrierten Beschuss eines Ziels mit Photonen kann wesentlich größerer Schaden erzielt werden als mit gewöhnlichen ballistischen Waffen.";
+$LOCA["de"]["SHORT_403"] = "Der schwere Laser ist eine konsequente Weiterentwicklung des leichten Lasers.";
+$LOCA["de"]["SHORT_404"] = "Die Gaußkanone beschleunigt tonnenschwere Geschosse unter gigantischem Energieaufwand.";
+$LOCA["de"]["SHORT_405"] = "Das Ionengeschütz schleudert eine Welle von Ionen, die ihrem Ziel erheblichen Schaden zufügt.";
+$LOCA["de"]["SHORT_406"] = "Plasmageschütze setzen die Kraft einer Sonneneruption frei und übertreffen in ihrer vernichtenden Wirkung sogar den Zerstörer.";
+$LOCA["de"]["SHORT_407"] = "Die kleine Schildkuppel umhüllt den ganzen Planeten mit einem Feld, das ungeheure Energiemengen absorbieren kann.";
+$LOCA["de"]["SHORT_408"] = "Die Weiterentwicklung der kleinen Schildkuppel kann wesentlich mehr Energie einsetzen, um Angriffe abzuwehren.";
+$LOCA["de"]["SHORT_502"] = "Abfangraketen zerstören angreifende Interplanetarraketen.";
+$LOCA["de"]["SHORT_503"] = "Interplanetarraketen zerstören die gegnerische Verteidigung.";
 
 ?>
