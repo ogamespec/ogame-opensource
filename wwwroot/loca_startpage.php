3255ab0fb3f015afc91fb9b25105dfd78afe5331
org <ogamespec@gmail.com>
2024-02-26 12:44:52 +0300

    Set "en" to default language for the startpage

--- wwwroot/loca_startpage.php
+++ wwwroot/loca_startpage.php
@@ -1,6 +1,6 @@
 <?php
 
-// Start page can be found on internet archive : http://archive.org
+// Start page can be found on internet archive: http://archive.org
 
 require_once "loca.php";
 
@@ -518,9 +518,35 @@ Soruyorsundur kendine bunu sana niye anlattigimi. Söyle desem: Sana burada beni
     }
 */
 
+// TODO: Залезть на archive.org и перетащить тексты для остальных языков.
+
+// <a href="#" onclick="javascript:setlang('br');"><img src="img/flags/br.gif" alt="Brazil" title="Brazil"></a>                      -- TODO
+// <a href="#" onclick="javascript:setlang('bg');"><img src="img/flags/bg.gif" alt="Bulgaria" title="Bulgaria"></a>             -- TODO
+// <a href="#" onclick="javascript:setlang('cn');"><img src="img/flags/cn.gif" alt="China" title="China"></a>                       -- TODO
+// <a href="#" onclick="javascript:setlang('cz');"><img src="img/flags/cz.gif" alt="Czech Republic" title="Czech Republic"></a>         -- TODO
+// <a href="#" onclick="javascript:setlang('de');"><img src="img/flags/de.gif" alt="Deutschland" title="Deutschland"></a>
+// <a href="#" onclick="javascript:setlang('dk');"><img src="img/flags/dk.gif" alt="Denmark" title="Denmark"></a>                          -- TODO
+// <a href="#" onclick="javascript:setlang('en');"><img src="img/flags/gb.gif" alt="English" title="English"></a>
+// <a href="#" onclick="javascript:setlang('es');"><img src="img/flags/es.gif" alt="Spain" title="Spain"></a>                          -- TODO
+// <a href="#" onclick="javascript:setlang('fr');"><img src="img/flags/fr.gif" alt="France" title="France"></a>                            -- TODO
+// <a href="#" onclick="javascript:setlang('gr');"><img src="img/flags/gr.gif" alt="Greece" title="Greece"></a>                 -- TODO
+// <a href="#" onclick="javascript:setlang('hu');"><img src="img/flags/hu.gif" alt="Hungary" title="Hungary"></a>               -- TODO
+// <a href="#" onclick="javascript:setlang('it');"><img src="img/flags/it.gif" alt="Italy" title="Italy"></a>                   -- TODO
+// <a href="#" onclick="javascript:setlang('jp');"><img src="img/flags/jp.gif" alt="Japan" title="Japan"></a>                   -- TODO
+// <a href="#" onclick="javascript:setlang('kr');"><img src="img/flags/kr.gif" alt="Korea" title="Korea"></a>                   -- TODO
+// <a href="#" onclick="javascript:setlang('nl');"><img src="img/flags/nl.gif" alt="Netherlands" title="Netherlands"></a>       -- TODO
+// <a href="#" onclick="javascript:setlang('no');"><img src="img/flags/no.gif" alt="Norway" title="Norway"></a>                    -- TODO
+// <a href="#" onclick="javascript:setlang('pl');"><img src="img/flags/pl.gif" alt="Poland" title="Poland"></a>                 -- TODO
+// <a href="#" onclick="javascript:setlang('pt');"><img src="img/flags/pt.gif" alt="Portugal" title="Portugal"></a>             -- TODO
+// <a href="#" onclick="javascript:setlang('ro');"><img src="img/flags/ro.gif" alt="Romania" title="Romania"></a>               -- TODO
+// <a href="#" onclick="javascript:setlang('ru');"><img src="img/flags/ru.gif" alt="Russia" title="Russia"></a>
+// <a href="#" onclick="javascript:setlang('sk');"><img src="img/flags/sk.gif" alt="Slovakia" title="Slovakia"></a>             -- TODO
+// <a href="#" onclick="javascript:setlang('se');"><img src="img/flags/se.gif" alt="Sweden" title="Sweden"></a>                 -- TODO
+// <a href="#" onclick="javascript:setlang('tr');"><img src="img/flags/tr.gif" alt="Turkey" title="Turkey"></a>                 -- TODO
+// <a href="#" onclick="javascript:setlang('tw');"><img src="img/flags/tw.gif" alt="Taiwan" title="Taiwan"></a>                 -- TODO
+
     // Язык стартовой страницы
-    if ( !isset ($_COOKIE['ogamelang']) ) $LocaLang = "ru";
+    if ( !isset ($_COOKIE['ogamelang']) ) $LocaLang = "en";
     else $LocaLang = $_COOKIE['ogamelang'];
 //    if ($LocaLang !== 'de' && $LocaLang !== 'en' && $LocaLang !== 'ru') $LocaLang = "en";        // restrict unsupported languages
-
 
