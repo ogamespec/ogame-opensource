3122154fc297d8361ed1da96f42b4f52603c5077
org <ogamespec@gmail.com>
2024-02-29 19:09:40 +0300

    check install in redir.php

--- game/redir.php
+++ game/redir.php
@@ -1,5 +1,13 @@
 <?php
 
+// Проверить, если файл конфигурации отсутствует - редирект на страницу установки игры.
+if ( !file_exists ("config.php"))
+{
+    echo "<html><head><meta http-equiv='refresh' content='0;url=install.php' /></head><body></body></html>";
+    ob_end_flush ();
+    exit ();
+}
+
 // Все ссылки из игры наружу проходят через это скрипт.
 // По идее тут могут быть фильтры нежелательных сайтов.
 
