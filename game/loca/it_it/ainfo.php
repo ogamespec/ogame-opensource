4f0ecb0c45d5c0623e1a90b7812ad9197475846a
org <ogamespec@gmail.com>
2024-03-02 15:36:31 +0300

    ainfo it loca

--- game/loca/it_it/ainfo.php
+++ game/loca/it_it/ainfo.php
@@ -2,11 +2,11 @@
 
 // Locale per la pagina di visualizzazione dell'alleanza esterna.
 
-$LOCA["it"]["AINFO_INFO"] = "Alliance Information";
-$LOCA["it"]["AINFO_TAG"] = "Tag";
-$LOCA["it"]["AINFO_NAME"] = "Name";
-$LOCA["it"]["AINFO_MEMBERS"] = "Member";
+$LOCA["it"]["AINFO_INFO"] = "Pagina Alleanza";
+$LOCA["it"]["AINFO_TAG"] = "Tag Alleanza:";
+$LOCA["it"]["AINFO_NAME"] = "Nome Alleanza:";
+$LOCA["it"]["AINFO_MEMBERS"] = "Membri:";
 $LOCA["it"]["AINFO_HOMEPAGE"] = "Homepage";
-$LOCA["it"]["AINFO_PREV"] = "ex.";
+$LOCA["it"]["AINFO_PREV"] = "Precedentemente:";
 
 ?>
