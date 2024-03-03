892f592c7d6583453af4425076905d997a41bdb1
org <ogamespec@gmail.com>
2024-03-03 10:13:47 +0300

    Update messages.php

--- game/loca/it_it/messages.php
+++ game/loca/it_it/messages.php
@@ -4,35 +4,35 @@
 
 // messages
 
-$LOCA["ru"]["MSG_FOLDER1"] = "Rapporti di spionaggio";
-$LOCA["ru"]["MSG_FOLDER2"] = "Rapporti di combattimento";
-$LOCA["ru"]["MSG_FOLDER3"] = "Rapporti di esplorazione";
-$LOCA["ru"]["MSG_FOLDER4"] = "Messaggi da alleanza";
-$LOCA["ru"]["MSG_FOLDER5"] = "Messaggi da giocatore";
-$LOCA["ru"]["MSG_FOLDER6"] = "Altri messaggi";
+$LOCA["it"]["MSG_FOLDER1"] = "Rapporti di spionaggio";
+$LOCA["it"]["MSG_FOLDER2"] = "Rapporti di combattimento";
+$LOCA["it"]["MSG_FOLDER3"] = "Rapporti di esplorazione";
+$LOCA["it"]["MSG_FOLDER4"] = "Messaggi da alleanza";
+$LOCA["it"]["MSG_FOLDER5"] = "Messaggi da giocatore";
+$LOCA["it"]["MSG_FOLDER6"] = "Altri messaggi";
 
-$LOCA["ru"]["MSG_DELETE_MARKED"] = "Cancella messaggi selezionati";
-$LOCA["ru"]["MSG_DELETE_UNMARKED"] = "Cancella messaggi non selezionati";
-$LOCA["ru"]["MSG_DELETE_SHOWN"] = "Cancella messaggi di questa pagina ";
-$LOCA["ru"]["MSG_DELETE_ALL"] = "Cancella tutti i messaggi";
+$LOCA["it"]["MSG_DELETE_MARKED"] = "Cancella messaggi selezionati";
+$LOCA["it"]["MSG_DELETE_UNMARKED"] = "Cancella messaggi non selezionati";
+$LOCA["it"]["MSG_DELETE_SHOWN"] = "Cancella messaggi di questa pagina ";
+$LOCA["it"]["MSG_DELETE_ALL"] = "Cancella tutti i messaggi";
 
-$LOCA["ru"]["MSG_MESSAGES"] = "Lista Messaggi";
-$LOCA["ru"]["MSG_FOLDER_SHOW"] = "Mostrare";
-$LOCA["ru"]["MSG_FOLDER_TYPE"] = "Tipo messaggio";
-$LOCA["ru"]["MSG_FOLDER_STAT"] = "Da leggere / Totale";
-$LOCA["ru"]["MSG_ACTION"] = "Azione";
-$LOCA["ru"]["MSG_DATE"] = "Data";
-$LOCA["ru"]["MSG_FROM"] = "Da";
-$LOCA["ru"]["MSG_SUBJ"] = "Oggetto";
+$LOCA["it"]["MSG_MESSAGES"] = "Lista Messaggi";
+$LOCA["it"]["MSG_FOLDER_SHOW"] = "Mostrare";
+$LOCA["it"]["MSG_FOLDER_TYPE"] = "Tipo messaggio";
+$LOCA["it"]["MSG_FOLDER_STAT"] = "Da leggere / Totale";
+$LOCA["it"]["MSG_ACTION"] = "Azione";
+$LOCA["it"]["MSG_DATE"] = "Data";
+$LOCA["it"]["MSG_FROM"] = "Da";
+$LOCA["it"]["MSG_SUBJ"] = "Oggetto";
 
-$LOCA["ru"]["MSG_SUBMIT"] = "ok";
+$LOCA["it"]["MSG_SUBMIT"] = "ok";
 
-$LOCA["ru"]["MSG_PARTIAL_ESPIONAGE"] = " Mostra solo parti dei rapporti di spionaggio ";
+$LOCA["it"]["MSG_PARTIAL_ESPIONAGE"] = " Mostra solo parti dei rapporti di spionaggio ";
 
-$LOCA["ru"]["MSG_REPORT"] = "Segnala";
-$LOCA["ru"]["MSG_OPER"] = "Operatori di gioco";
-$LOCA["ru"]["MSG_OPER_TEXT"] = "Domanda dal #1 dell'universo #2";
-$LOCA["ru"]["MSG_OPER_PM"] = "Scrivi un messaggio";
+$LOCA["it"]["MSG_REPORT"] = "Segnala";
+$LOCA["it"]["MSG_OPER"] = "Operatori di gioco";
+$LOCA["it"]["MSG_OPER_TEXT"] = "Domanda dal #1 dell'universo #2";
+$LOCA["it"]["MSG_OPER_PM"] = "Scrivi un messaggio";
 
 // writemessages
 
