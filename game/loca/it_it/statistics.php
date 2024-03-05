a2255ef5176b8a709020399ad2939b9a5200a400
org <ogamespec@gmail.com>
2024-03-05 22:47:39 +0300

    statistics loca

---
+++ game/loca/it_it/statistics.php
@@ -0,0 +1,24 @@
+<?php
+
+// Statistica.
+
+$LOCA["it"]["STAT_HEAD"] = "Statistiche (Aggiornamento: #1)";
+$LOCA["it"]["STAT_WHO"] = "mostra";
+$LOCA["it"]["STAT_PLAYER"] = "Giocatori";
+$LOCA["it"]["STAT_ALLY"] = "Alleanze";
+$LOCA["it"]["STAT_TYPE"] = "per";
+$LOCA["it"]["STAT_BY_POINTS"] = "Punteggi";
+$LOCA["it"]["STAT_BY_FLEETS"] = "Flotta";
+$LOCA["it"]["STAT_BY_RESEARCH"] = "Ricerche";
+$LOCA["it"]["STAT_ON_PLACE"] = "nelle posizioni";
+$LOCA["it"]["STAT_OWN_POSITION"] = "[Posizione propria]";
+$LOCA["it"]["STAT_SUBMIT"] = "Invia";
+$LOCA["it"]["STAT_PLACE"] = "Posizione";
+$LOCA["it"]["STAT_MEMBERS"] = "Membri";
+$LOCA["it"]["STAT_1K_POINTS"] = "Punteggio";
+$LOCA["it"]["STAT_PER_PLAYER"] = "Per membro";
+$LOCA["it"]["STAT_POINTS"] = "Punti";
+$LOCA["it"]["STAT_DATE_CHANGE"] = "Da #1";
+$LOCA["it"]["STAT_WRITE_MESSAGE"] = "Scrivi un messaggio";
+
+?>
