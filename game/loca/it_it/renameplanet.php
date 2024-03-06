f0b33c2428994819b0ad4be7de36ebae5e9b977d
org <ogamespec@gmail.com>
2024-03-06 08:05:24 +0300

    more it loca

---
+++ game/loca/it_it/renameplanet.php
@@ -0,0 +1,21 @@
+<?php
+
+// Menu del pianeta.
+
+$LOCA["it"]["REN_TITLE"] = "Rinomina/Abbandona colonia";
+$LOCA["it"]["REN_WARNING"] = "Abbandona pianeta";
+$LOCA["it"]["REN_DELETE_INFO"] = "Abbandono pianeta alle coordinate #1 proteggere con password";
+$LOCA["it"]["REN_PASSWORD"] = "Password";
+$LOCA["it"]["REN_DELETE_PLANET"] = "Abbandona colonia!";
+$LOCA["it"]["REN_ABANDON_COLONY"] = "Abbandona colonia";
+$LOCA["it"]["REN_RENAME"] = "Rinomina";
+$LOCA["it"]["REN_ERROR_PASSWORD"] = "Password errata.<BR><BR>  Se hai dimenticato la password clicca #1QUI#2 <BR><BR>  o provare #3 ancora una volta#4 .";
+$LOCA["it"]["REN_ERROR_HOME_PLANET"] = "Non puoi abbandonare il pianeta madre!";
+$LOCA["it"]["REN_ERROR_FLEET_INCOME"] = "Le vostre flotte sono ancora in viaggio verso questo pianeta!";
+$LOCA["it"]["REN_ERROR_FLEET_OUTCOME"] = "Le flotte di questo pianeta non sono ancora tornate!";
+$LOCA["it"]["REN_PLANET_INFO"] = "Rinomina o abbandona pianeta";
+$LOCA["it"]["REN_COORD"] = "Coordinate";
+$LOCA["it"]["REN_NAME"] = "Nome";
+$LOCA["it"]["REN_ACTIONS"] = "Azioni";
+
+?>
