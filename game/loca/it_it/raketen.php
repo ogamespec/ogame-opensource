643664f8f7b29f29c2af024ade82c22b154b1225
org <ogamespec@gmail.com>
2024-03-14 18:03:38 +0300

    raketen it loca

--- game/loca/it_it/raketen.php
+++ game/loca/it_it/raketen.php
@@ -2,12 +2,12 @@
 
 // Rapporti di attacco missilistico.
 
-$LOCA["it"]["RAK_MSG_SUBJ"] = "Ракетная атака";
-$LOCA["it"]["RAK_TITLE"] = "Поражённая оборона";
-$LOCA["it"]["RAK_DEF_TEXT1"] = "#1 ракетам из общего числа выпущенных ракет с планеты";
-$LOCA["it"]["RAK_DEF_TEXT2"] = "удалось попасть на Вашу планету";
-$LOCA["it"]["RAK_DEF_TEXT3"] = "#1 ракет(-ы) было уничтожено Вашими ракетами-перехватчиками";
-$LOCA["it"]["RAK_ATT_TEXT1"] = "#1 ракет(ы) с Вашей планеты";
-$LOCA["it"]["RAK_ATT_TEXT2"] = "ударили по планете";
+$LOCA["it"]["RAK_MSG_SUBJ"] = "Attacco missilistico";
+$LOCA["it"]["RAK_TITLE"] = "Difese rimaste";
+$LOCA["it"]["RAK_DEF_TEXT1"] = "#1 missili interplanetari partiti dal pianeta";
+$LOCA["it"]["RAK_DEF_TEXT2"] = "hanno colpito il tuo pianeta";
+$LOCA["it"]["RAK_DEF_TEXT3"] = "#1 missili interplanetari sono stati distrutti dai tuoi missili anti-balistici";
+$LOCA["it"]["RAK_ATT_TEXT1"] = "#1 missili dal vostro pianeta.";
+$LOCA["it"]["RAK_ATT_TEXT2"] = "colpire il pianeta";
 
 ?>
