00a540a101a5c9bba9705fe7a2f27c7a43cfb65b
org <ogamespec@gmail.com>
2024-04-29 19:12:00 +0300

    infos de/en loca

---
+++ game/loca/de_de/infos.php
@@ -0,0 +1,60 @@
+<?php
+
+// Technologische Informationen.
+// TODO: Es wurde maschinelle Übersetzung verwendet.
+
+$LOCA["de"]["INFO_REPAIR"] = "Die Fähigkeit zur Wiederherstellung der Verteidigung nach einem Kampf beträgt bis zu 1%.";
+
+$LOCA["de"]["INFO_RAPID_IN1"] = "";
+$LOCA["de"]["INFO_RAPID_IN2"] = " eine Salve trifft die Einheit #1 des Typs";
+$LOCA["de"]["INFO_RAPID_OUT1"] = "Eine Salve schlägt ein: ";
+$LOCA["de"]["INFO_RAPID_OUT2"] = " - #1 Einheiten";
+$LOCA["de"]["INFO_FLEET"] = "Informationen zur Flotte:";
+$LOCA["de"]["INFO_NAME"] = "Titel";
+$LOCA["de"]["INFO_STRUCTURE"] = "Struktur";
+$LOCA["de"]["INFO_SHIELD"] = "Leistung des Schildes";
+$LOCA["de"]["INFO_ATTACK"] = "Bewertung von Angriffen";
+$LOCA["de"]["INFO_CARGO"] = "Tragfähigkeit";
+$LOCA["de"]["INFO_UNITS"] = "&nbsp;Einheit.";
+$LOCA["de"]["INFO_BASE_SPEED"] = "Anfangsgeschwindigkeit";
+$LOCA["de"]["INFO_BASE_CONS"] = "Kraftstoffverbrauch (Deuterium)";
+$LOCA["de"]["INFO_DEFENSE"] = "Informationen über Verteidigungsmaßnahmen:";
+$LOCA["de"]["INFO_LEVEL"] = "Stufe";
+$LOCA["de"]["INFO_PROD"] = "Produktion pro Stunde";
+$LOCA["de"]["INFO_DIFF"] = "Differenz";
+$LOCA["de"]["INFO_ENERGY"] = "Energiebilanz";
+$LOCA["de"]["INFO_CONS_DEUT"] = "Deuteriumverbrauch";
+$LOCA["de"]["INFO_STORAGE"] = "Kapazität";
+
+// allianzdepot
+
+$LOCA["de"]["INFO_DEPOT_CAPACITY"] = "Kapazität: #1/#2";
+$LOCA["de"]["INFO_DEPOT_FLEET"] = "Flotte #1:";
+$LOCA["de"]["INFO_DEPOT_SUPPLY"] = "Laden";
+$LOCA["de"]["INFO_DEPOT_SECONDS"] = "#1 sec";
+$LOCA["de"]["INFO_DEPOT_HOUR"] = "St";
+$LOCA["de"]["INFO_DEPOT_PER_HOUR"] = "Kosten #1 / Std.";
+$LOCA["de"]["INFO_DEPOT_SUBMIT"] = "Eine Rakete mit Vorräten starten";
+
+// rocket silo
+
+$LOCA["de"]["INFO_SILO_INFO"] = "Ihr Tresor kann interplanetarische Raketen der Klasse #1 oder Abfangraketen der Klasse #2 aufnehmen.";
+$LOCA["de"]["INFO_SILO_TYPE"] = "Typ";
+$LOCA["de"]["INFO_SILO_AMOUNT"] = "Packungen";
+$LOCA["de"]["INFO_SILO_DEMOLISH"] = "Abreißen";
+$LOCA["de"]["INFO_SILO_SUBMIT"] = "Ausführen";
+
+// phalanx
+
+$LOCA["de"]["INFO_PHALANX_LEVEL"] = "Stufe";
+$LOCA["de"]["INFO_PHALANX_RADIUS"] = "Aktionsradius";
+
+$LOCA["de"]["INFO_DEMOLISH_TITLE"] = "Abreißen: #1 Level #2 zerstören?";
+$LOCA["de"]["INFO_DEMOLISH_RES"] = "Erforderlich ";
+$LOCA["de"]["INFO_DEMOLISH_M"] = "Metall:";
+$LOCA["de"]["INFO_DEMOLISH_K"] = "Kristall:";
+$LOCA["de"]["INFO_DEMOLISH_D"] = "Deuterium:";
+$LOCA["de"]["INFO_DEMOLISH_DURATION"] = "Dauer des Abrisses:";
+$LOCA["de"]["INFO_DEMOLISH_DEFENSE"] = "Verteidigungsmaßnahmen";
+
+?>
