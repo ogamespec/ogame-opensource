df5eacdaaa7075cf64e80dbdfb685601ff098f54
org <ogamespec@gmail.com>
2024-04-29 20:42:03 +0300

    events loca

--- game/loca/de_de/events.php
+++ game/loca/de_de/events.php
@@ -2,53 +2,53 @@
 
 // Liste der Ereignisse (Übersicht und Phalanx)
 
-$LOCA["de"]["EVENT_FLEET_COUNT"] = "Численность кораблей";
-$LOCA["de"]["EVENT_WRITE"] = "Написать сообщение";
-$LOCA["de"]["EVENT_FROM_PLANET"] = "планеты";
-$LOCA["de"]["EVENT_TO_PLANET"] = "планету";
-$LOCA["de"]["EVENT_CARGO"] = "Транспорт";
+$LOCA["de"]["EVENT_FLEET_COUNT"] = "Anzahl der Schiffe";
+$LOCA["de"]["EVENT_WRITE"] = "Nachricht schreiben";
+$LOCA["de"]["EVENT_FROM_PLANET"] = "Planeten";
+$LOCA["de"]["EVENT_TO_PLANET"] = "Planet";
+$LOCA["de"]["EVENT_CARGO"] = "Transport";
 
-$LOCA["de"]["EVENT_FLEET_OWN"] = "Ваш #1флот";
-$LOCA["de"]["EVENT_FLEET_ENEMY"] = "Боевой #1флот";
-$LOCA["de"]["EVENT_FLEET_FRIEND"] = "Мирный #1флот";
-$LOCA["de"]["EVENT_FLEET_HOLD"] = "#1 удерживает альянсовый #2флот";
-$LOCA["de"]["EVENT_FLEET_ACS_HEAD"] = "Альянсовый #1флот";
+$LOCA["de"]["EVENT_FLEET_OWN"] = "Ihre #1Flotte";
+$LOCA["de"]["EVENT_FLEET_ENEMY"] = "Kampf #1Flotte";
+$LOCA["de"]["EVENT_FLEET_FRIEND"] = "Friedliche #1Flotte";
+$LOCA["de"]["EVENT_FLEET_HOLD"] = "#1 hält die #2-Flotte der Allianz";
+$LOCA["de"]["EVENT_FLEET_ACS_HEAD"] = "Allianz #1Flotte";
 
-$LOCA["de"]["EVENT_MISSION"] = "Задание";
+$LOCA["de"]["EVENT_MISSION"] = "Zuweisung";
 
-$LOCA["de"]["EVENT_FROM_TO"] = " с #1 отправлен на #2";
-$LOCA["de"]["EVENT_FROM_RETURN_TO"] = ", отправленный с #1, возвращается на #2";
-$LOCA["de"]["EVENT_PLAYER_FROM_TO"] = " игрока #1 с #2 отправлен на #3";
-$LOCA["de"]["EVENT_HOLD_FROM_ONTO"] = ", отправленный с #1, находится на орбите #2";
-$LOCA["de"]["EVENT_FROM_TO_ORBIT"] = " с #1 на орбите #2";
+$LOCA["de"]["EVENT_FROM_TO"] = " von #1 gesendet an #2";
+$LOCA["de"]["EVENT_FROM_RETURN_TO"] = ", von #1 gesendet wird an #2 zurückgeschickt";
+$LOCA["de"]["EVENT_PLAYER_FROM_TO"] = " Spieler #1 von #2 nach #3 geschickt";
+$LOCA["de"]["EVENT_HOLD_FROM_ONTO"] = ", von #1 gesendet wird, befindet sich in der Umlaufbahn #2.";
+$LOCA["de"]["EVENT_FROM_TO_ORBIT"] = " mit #1 in der Umlaufbahn #2";
 // Für die Kolonisierung
-$LOCA["de"]["EVENT_COLONY_FROM_TO"] = " с #1 отправлен на позицию #2";
-$LOCA["de"]["EVENT_COLONY_FROM_RETURN_TO"] = ", отправленный с позиции #1, возвращается на #2";
+$LOCA["de"]["EVENT_COLONY_FROM_TO"] = " von #1 an Position #2 gesendet";
+$LOCA["de"]["EVENT_COLONY_FROM_RETURN_TO"] = ", von Position #1 gesendet, wird an Position #2 zurückgeschickt";
 // Für die Expedition
-$LOCA["de"]["EVENT_EXPO_FROM_TO"] = " отправленный с #1 достигает позиции #2";
-$LOCA["de"]["EVENT_EXPO_RETURN_FROM_TO"] = " возвращается на #1 после приказа #2";
-$LOCA["de"]["EVENT_EXPO_FROM_ONTO"] = ", отправленный с #1 исследует позицию #2";
+$LOCA["de"]["EVENT_EXPO_FROM_TO"] = " gesendet von #1 erreicht Position #2";
+$LOCA["de"]["EVENT_EXPO_RETURN_FROM_TO"] = " kehrt nach Auftrag #2 zu #1 zurück";
+$LOCA["de"]["EVENT_EXPO_FROM_ONTO"] = ", von #1 gesendet wird, untersucht Position #2";
 // Für die Phalanx
-$LOCA["de"]["EVENT_FROM_TO_PHALANX"] = " игрока #1 с планеты #2 отправлен на #3"; 		// fliegt auf einer Phalanx davon
-$LOCA["de"]["EVENT_FROM_RETURN_TO_PHALANX"] = " возвратится с #1 на #2";   			// kommt zurück auf die Phalanx
-$LOCA["de"]["EVENT_EXPO_FROM_ONTO_PHALANX"] = " игрока #1 с #2 исследует позицию #3";  	// erforscht eine Phalanx-Expedition
+$LOCA["de"]["EVENT_FROM_TO_PHALANX"] = " Spieler #1 vom Planeten #2 wurde nach #3 geschickt."; 		// fliegt auf einer Phalanx davon
+$LOCA["de"]["EVENT_FROM_RETURN_TO_PHALANX"] = " wird von #1 auf #2 wechseln";   			// kommt zurück auf die Phalanx
+$LOCA["de"]["EVENT_EXPO_FROM_ONTO_PHALANX"] = " Spieler #1 mit #2 erkundet Position #3";  	// erforscht eine Phalanx-Expedition
 
 // Raketenangriff
-$LOCA["de"]["EVENT_RAK"] = "Ракетная атака (#1) с #2 на #3";
-$LOCA["de"]["EVENT_RAK_TARGET"] = "Основная цель";
+$LOCA["de"]["EVENT_RAK"] = "Raketenangriff (#1) von #2 auf #3";
+$LOCA["de"]["EVENT_RAK_TARGET"] = "Hauptziel";
 
 // Aufgaben
 
-$LOCA["de"]["EVENT_M_ATTACK"] = "Атаковать";
-$LOCA["de"]["EVENT_M_ACS_ATTACK"] = "Совместная атака";
-$LOCA["de"]["EVENT_M_TRANSPORT"] = "Транспорт";
-$LOCA["de"]["EVENT_M_DEPLOY"] = "Оставить";
-$LOCA["de"]["EVENT_M_HOLD"] = "Держаться";
-$LOCA["de"]["EVENT_M_SPY"] = "Шпионаж";
-$LOCA["de"]["EVENT_M_COLONY"] = "Колонизировать";
-$LOCA["de"]["EVENT_M_RECYCLE"] = "Переработать";
-$LOCA["de"]["EVENT_M_DESTROY"] = "Уничтожить";
-$LOCA["de"]["EVENT_M_ACS_ATTACK_HEAD"] = "Атаковать";  // ACS Train
-$LOCA["de"]["EVENT_M_EXPO"] = "Экспедиция";
+$LOCA["de"]["EVENT_M_ATTACK"] = "Angriff";
+$LOCA["de"]["EVENT_M_ACS_ATTACK"] = "Gemeinsamer Angriff";
+$LOCA["de"]["EVENT_M_TRANSPORT"] = "Transport";
+$LOCA["de"]["EVENT_M_DEPLOY"] = "Belassen";
+$LOCA["de"]["EVENT_M_HOLD"] = "Festhalten";
+$LOCA["de"]["EVENT_M_SPY"] = "Spionage";
+$LOCA["de"]["EVENT_M_COLONY"] = "Kolonisieren";
+$LOCA["de"]["EVENT_M_RECYCLE"] = "Recyceln";
+$LOCA["de"]["EVENT_M_DESTROY"] = "Zerstören";
+$LOCA["de"]["EVENT_M_ACS_ATTACK_HEAD"] = "Angriff";  // ACS Train
+$LOCA["de"]["EVENT_M_EXPO"] = "Expedition";
 
 ?>
