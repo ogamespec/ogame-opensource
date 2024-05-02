891625ba0717e5deee642a711817f11b37b0e2fb
org <ogamespec@gmail.com>
2024-05-02 21:12:17 +0300

    Update pranger.php
    Found pranger picture

--- game/loca/en_en/pranger.php
+++ game/loca/en_en/pranger.php
@@ -2,14 +2,14 @@
 
 // Pillar of Shame.
 
-$LOCA["en"]["PRANGER_TITLE"] = "Pillar of Shame #1";
-$LOCA["en"]["PRANGER_INFO"] = "It says who is blocked, why and for how long. 
+$LOCA["en"]["PRANGER_TITLE"] = "OGame Pillory Universe #1";
+$LOCA["en"]["PRANGER_INFO"] = "Here is a list of players who have been banned, until when and for what reason. 
 <br />Blocking by the Admin Council and the system is NOT negotiable. 
 <br />Attention! Your message will be processed faster with an automatic subject line.";
-$LOCA["en"]["PRANGER_WHEN"] = "When";
-$LOCA["en"]["PRANGER_OPER"] = "Who blocked";
+$LOCA["en"]["PRANGER_WHEN"] = "Ban Date";
+$LOCA["en"]["PRANGER_OPER"] = "Admin Name";
 $LOCA["en"]["PRANGER_USER"] = "Player Name";
-$LOCA["en"]["PRANGER_UNTIL"] = "Blocked until";
+$LOCA["en"]["PRANGER_UNTIL"] = "Blocked Until";
 $LOCA["en"]["PRANGER_REASON"] = "Reason";
 $LOCA["en"]["PRANGER_PREV"] = "Previous #1";
 $LOCA["en"]["PRANGER_NEXT"] = "Next #1";
