dd10e9fb5e46fffd3201959d51d28f8e0122e5f2
org <ogamespec@gmail.com>
2024-05-03 14:11:45 +0300

    Update resources.php

--- game/loca/en_en/resources.php
+++ game/loca/en_en/resources.php
@@ -2,16 +2,18 @@
 
 // Resources
 
-$LOCA["en"]["RES_FACTOR"] = "Production Factor: #1";
-$LOCA["en"]["RES_CALCULATE"] = "calculate";
+// 03.05.2024: Got the strings from the current official game
+
+$LOCA["en"]["RES_FACTOR"] = "Production factor: #1";
+$LOCA["en"]["RES_CALCULATE"] = "Recalculate";
 $LOCA["en"]["RES_INFO"] = "There is unrest on the planet because the empire is too large.";
-$LOCA["en"]["RES_PROD"] = "Resource production on planet";
+$LOCA["en"]["RES_PROD"] = "Resource settings on planet";
 $LOCA["en"]["RES_BASIC"] = "Basic Income";
-$LOCA["en"]["RES_LEVEL"] = "level #1";
+$LOCA["en"]["RES_LEVEL"] = "Level #1";
 $LOCA["en"]["RES_AMOUNT"] = "#1 available";
-$LOCA["en"]["RES_CAPACITY"] = "Storage Capacity";
-$LOCA["en"]["RES_PER_HOUR"] = "Total:";
-$LOCA["en"]["RES_PER_DAY"] = "Res per day:";
-$LOCA["en"]["RES_PER_WEEK"] = "Res per week:";
+$LOCA["en"]["RES_CAPACITY"] = "Storage capacity";
+$LOCA["en"]["RES_PER_HOUR"] = "Total per hour:";
+$LOCA["en"]["RES_PER_DAY"] = "Total per day:";
+$LOCA["en"]["RES_PER_WEEK"] = "Total per week:";
 
 ?>
