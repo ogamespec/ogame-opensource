77776b7752912a34c0b8cb501e76de1cfd759961
org <ogamespec@gmail.com>
2024-05-04 22:44:21 +0300

    reverted technames it after discussion on pizza channel

--- game/loca/it_it/technames.php
+++ game/loca/it_it/technames.php
@@ -1,20 +1,21 @@
 <?php
 
 // Oggetti di gioco
-// 04.05.2024: Sincronizzato con i nomi ufficiali
+// ⚠️ Gli attuali nomi ufficiali delle tecnologie su OGame.it sono diversi, ma i veterani affermano che sono diversi da quelli che erano nella 0.84.
+// Quindi lasceremo questi nomi al procedimento.
 
-$LOCA["it"]["NAME_1"] = "Miniera di Metallo";
-$LOCA["it"]["NAME_2"] = "Miniera di Cristalli";
+$LOCA["it"]["NAME_1"] = "Miniera di metallo";
+$LOCA["it"]["NAME_2"] = "Miniera di cristalli";
 $LOCA["it"]["NAME_3"] = "Sintetizzatore di deuterio";
 $LOCA["it"]["NAME_4"] = "Centrale solare";
-$LOCA["it"]["NAME_12"] = "Centrale a Fusione";
-$LOCA["it"]["NAME_14"] = "Fabbrica dei Robot";
-$LOCA["it"]["NAME_15"] = "Fabbrica dei Naniti";
-$LOCA["it"]["NAME_21"] = "Cantiere Spaziale";
+$LOCA["it"]["NAME_12"] = "Centrale a fusione";
+$LOCA["it"]["NAME_14"] = "Fabbrica dei robot";
+$LOCA["it"]["NAME_15"] = "Fabbrica dei naniti";
+$LOCA["it"]["NAME_21"] = "Cantiere spaziale";
 $LOCA["it"]["NAME_22"] = "Deposito di metallo";
 $LOCA["it"]["NAME_23"] = "Deposito di cristalli";
 $LOCA["it"]["NAME_24"] = "Cisterna di deuterio";
-$LOCA["it"]["NAME_31"] = "Laboratorio di Ricerca";
+$LOCA["it"]["NAME_31"] = "Laboratorio di ricerca.";
 $LOCA["it"]["NAME_33"] = "Terraformer";
 $LOCA["it"]["NAME_34"] = "Base di appoggio";
 $LOCA["it"]["NAME_41"] = "Avamposto lunare";
@@ -22,47 +23,47 @@ $LOCA["it"]["NAME_42"] = "Falange di sensori";
 $LOCA["it"]["NAME_43"] = "Portale iperspaziale";
 $LOCA["it"]["NAME_44"] = "Base missilistica";
 
-$LOCA["it"]["NAME_106"] = "Tecnologia per lo spionaggio";
+$LOCA["it"]["NAME_106"] = "Tecnologie per lo spionaggio";
 $LOCA["it"]["NAME_108"] = "Tecnologia informatica";
 $LOCA["it"]["NAME_109"] = "Tecnologia delle armi";
 $LOCA["it"]["NAME_110"] = "Tecnologia degli scudi";
 $LOCA["it"]["NAME_111"] = "Tecnologia delle corazze";
-$LOCA["it"]["NAME_113"] = "Tecnologia Energetica";
+$LOCA["it"]["NAME_113"] = "Tecnologia energetica";
 $LOCA["it"]["NAME_114"] = "Tecnologia iperspaziale";
 $LOCA["it"]["NAME_115"] = "Propulsore a combustione";
 $LOCA["it"]["NAME_117"] = "Propulsore a impulso";
 $LOCA["it"]["NAME_118"] = "Propulsore iperspaziale";
-$LOCA["it"]["NAME_120"] = "Tecnologia dei Laser";
+$LOCA["it"]["NAME_120"] = "Tecnologia dei laser";
 $LOCA["it"]["NAME_121"] = "Tecnologia ionica";
-$LOCA["it"]["NAME_122"] = "Tecnologia del plasma";
+$LOCA["it"]["NAME_122"] = "Tecnologia dei plasmi";
 $LOCA["it"]["NAME_123"] = "Rete interplanetaria di ricerca";
 $LOCA["it"]["NAME_124"] = "Tecnologia di spedizione";
-$LOCA["it"]["NAME_199"] = "Tecnologia Gravitonica";
+$LOCA["it"]["NAME_199"] = "Tecnologia gravitonica";
 
 $LOCA["it"]["NAME_202"] = "Cargo leggero";
-$LOCA["it"]["NAME_203"] = "Cargo Pesante";
-$LOCA["it"]["NAME_204"] = "Caccia Leggero";
-$LOCA["it"]["NAME_205"] = "Caccia Pesante";
+$LOCA["it"]["NAME_203"] = "Cargo pesante";
+$LOCA["it"]["NAME_204"] = "Caccia leggero";
+$LOCA["it"]["NAME_205"] = "Caccia pesante";
 $LOCA["it"]["NAME_206"] = "Incrociatore";
 $LOCA["it"]["NAME_207"] = "Nave da battaglia";
 $LOCA["it"]["NAME_208"] = "Colonizzatrice";
-$LOCA["it"]["NAME_209"] = "Riciclatrici";
+$LOCA["it"]["NAME_209"] = "Riciclatrice";
 $LOCA["it"]["NAME_210"] = "Sonda spia";
 $LOCA["it"]["NAME_211"] = "Bombardiere";
-$LOCA["it"]["NAME_212"] = "Satellite Solare";
+$LOCA["it"]["NAME_212"] = "Satellite solare";
 $LOCA["it"]["NAME_213"] = "Corazzata";
-$LOCA["it"]["NAME_214"] = "Morte Nera";
-$LOCA["it"]["NAME_215"] = "Incrociatore da Battaglia";
+$LOCA["it"]["NAME_214"] = "Morte nera";
+$LOCA["it"]["NAME_215"] = "Incrociatore da battaglia";
 
 $LOCA["it"]["NAME_401"] = "Lanciamissili";
 $LOCA["it"]["NAME_402"] = "Laser leggero";
 $LOCA["it"]["NAME_403"] = "Laser pesante";
-$LOCA["it"]["NAME_404"] = "Cannone Gauss";
+$LOCA["it"]["NAME_404"] = "Cannone gauss";
 $LOCA["it"]["NAME_405"] = "Cannone ionico";
-$LOCA["it"]["NAME_406"] = "Cannone al Plasma";
-$LOCA["it"]["NAME_407"] = "Cupola scudo piccola";
+$LOCA["it"]["NAME_406"] = "Cannone al plasma";
+$LOCA["it"]["NAME_407"] = "Cupola scudo";
 $LOCA["it"]["NAME_408"] = "Cupola scudo potenziata";
-$LOCA["it"]["NAME_502"] = "Missili anti balistici";
-$LOCA["it"]["NAME_503"] = "Missili Interplanetari";
+$LOCA["it"]["NAME_502"] = "Missili anti-balistici";
+$LOCA["it"]["NAME_503"] = "Missili interplanetari";
 
 ?>
