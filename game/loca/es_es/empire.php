893eae47f4b177ffcc5c98dd50965237fdf5d5a8
Intervencion <13396742+Intervencion@users.noreply.github.com>
2025-05-08 00:04:53 +0200

    Update empire.php

--- game/loca/es_es/empire.php
+++ game/loca/es_es/empire.php
@@ -2,19 +2,19 @@
 
 // Empire.
 
-$LOCA["es"]["EMPIRE_OVERVIEW"] = "Empire Overview";
-$LOCA["es"]["EMPIRE_PLANETS"] = "Planets";
-$LOCA["es"]["EMPIRE_MOONS"] = "Moons";
-$LOCA["es"]["EMPIRE_RES"] = "Resources";
-$LOCA["es"]["EMPIRE_BUILDINGS"] = "Buildings";
-$LOCA["es"]["EMPIRE_RESEARCH"] = "Research";
-$LOCA["es"]["EMPIRE_FLEET"] = "Ships";
-$LOCA["es"]["EMPIRE_DEFENSE"] = "Defense";
-$LOCA["es"]["EMPIRE_NAME"] = "Name";
-$LOCA["es"]["EMPIRE_COORD"] = "Coordinates";
-$LOCA["es"]["EMPIRE_FIELDS"] = "Fields";
-$LOCA["es"]["EMPIRE_ACTION"] = "click 1x for construction menu, 2x to start construction";
-$LOCA["es"]["EMPIRE_AVG"] = "Average per planet";
-$LOCA["es"]["EMPIRE_SUM"] = "Sum";
+$LOCA["es"]["EMPIRE_OVERVIEW"] = "Visión del Imperio";
+$LOCA["es"]["EMPIRE_PLANETS"] = "Planetas";
+$LOCA["es"]["EMPIRE_MOONS"] = "Lunas";
+$LOCA["es"]["EMPIRE_RES"] = "Recursos";
+$LOCA["es"]["EMPIRE_BUILDINGS"] = "Edificios";
+$LOCA["es"]["EMPIRE_RESEARCH"] = "Tecnologías";
+$LOCA["es"]["EMPIRE_FLEET"] = "Naves";
+$LOCA["es"]["EMPIRE_DEFENSE"] = "Defensas";
+$LOCA["es"]["EMPIRE_NAME"] = "Nombre";
+$LOCA["es"]["EMPIRE_COORD"] = "Coordenadas";
+$LOCA["es"]["EMPIRE_FIELDS"] = "Campos";
+$LOCA["es"]["EMPIRE_ACTION"] = "Click 1x para abrir el menú de construcción, 2x para empezar la construcción"; //hover
+$LOCA["es"]["EMPIRE_AVG"] = "Promedio por planeta"; //hover
+$LOCA["es"]["EMPIRE_SUM"] = "Total";
 
 ?>
