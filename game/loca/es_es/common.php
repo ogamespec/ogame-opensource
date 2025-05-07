0f3c41c628a6dda1fac41551c54cac14e5788810
Intervencion <13396742+Intervencion@users.noreply.github.com>
2025-05-07 23:54:12 +0200

    Update common.php

--- game/loca/es_es/common.php
+++ game/loca/es_es/common.php
@@ -2,45 +2,45 @@
 
 // Common strings. Occur on most pages.
 
-$LOCA["es"]["PAGE_TITLE"] = "Uni#1 OGame";
+$LOCA["es"]["PAGE_TITLE"] = "Universo #1 OGame";
 
 $LOCA["es"]["METAL" ] = "Metal";
-$LOCA["es"]["CRYSTAL" ] = "Crystal";
-$LOCA["es"]["DEUTERIUM" ] = "Deuterium";
-$LOCA["es"]["ENERGY" ] = "Energy";
-$LOCA["es"]["DM" ] = "Dark Matter";
+$LOCA["es"]["CRYSTAL" ] = "Cristal";
+$LOCA["es"]["DEUTERIUM" ] = "Deuterio";
+$LOCA["es"]["ENERGY" ] = "Energía";
+$LOCA["es"]["DM" ] = "Materia Oscura";
 
-$LOCA["es"]["PLANET_HOME"] = "Homeplanet";
-$LOCA["es"]["PLANET_COLONY"] = "Colony";
-$LOCA["es"]["PLANET_PHANTOM"] = "Planet";
-$LOCA["es"]["PLANET_ABANDONED"] = "Planet abandoned";
-$LOCA["es"]["PLANET_DESTROYED"] = "Planet destroyed";
-$LOCA["es"]["MOON"] = "Moon";
-$LOCA["es"]["MOON_DESTROYED"] = "Abandoned";
-$LOCA["es"]["DEBRIS"] = "Debris Field";
-$LOCA["es"]["FAR_SPACE"] = "Far space";
+$LOCA["es"]["PLANET_HOME"] = "Planeta principal";
+$LOCA["es"]["PLANET_COLONY"] = "Colonia";
+$LOCA["es"]["PLANET_PHANTOM"] = "Planeta";
+$LOCA["es"]["PLANET_ABANDONED"] = "Planeta abandonado";
+$LOCA["es"]["PLANET_DESTROYED"] = "Planeta destruido";
+$LOCA["es"]["MOON"] = "Luna";
+$LOCA["es"]["MOON_DESTROYED"] = "Abandonado";
+$LOCA["es"]["DEBRIS"] = "Campo de escombros";
+$LOCA["es"]["FAR_SPACE"] = "Espacio exterior";
 
-$LOCA["es"]["TIME_DAYS"] = "d";		// TODO: Not sure
+$LOCA["es"]["TIME_DAYS"] = "d";
 $LOCA["es"]["TIME_HOUR"] = "h";
 $LOCA["es"]["TIME_MIN"] = "m";
 $LOCA["es"]["TIME_SEC"] = "s";
 
 // Commander and officers.
 
-$LOCA["es"]["PR_COMA" ] = "OGame Commander";
-$LOCA["es"]["PR_ADMIRAL" ] = "Fleet Admiral";
-$LOCA["es"]["PR_ENGINEER" ] = "Engineer";
-$LOCA["es"]["PR_GEOLOGIST" ] = "Geologist";
-$LOCA["es"]["PR_TECHNO" ] = "Technocrat";
+$LOCA["es"]["PR_COMA" ] = "Comandante";
+$LOCA["es"]["PR_ADMIRAL" ] = "Almirante";
+$LOCA["es"]["PR_ENGINEER" ] = "Ingeniero";
+$LOCA["es"]["PR_GEOLOGIST" ] = "Geólogo";
+$LOCA["es"]["PR_TECHNO" ] = "Tecnócrata";
 
-$LOCA["es"]["PR_ADMIRAL_INFO" ] = "&amp;nbsp;max. fleet slots +2";
-$LOCA["es"]["PR_ENGINEER_INFO" ] = "Reduces the losses of the defense by half&lt;br&gt; +10% more energy";
-$LOCA["es"]["PR_GEOLOGIST_INFO" ] = "+10% mine production";
-$LOCA["es"]["PR_TECHNO_INFO" ] = "+2 espionage level for probes, 25% less research time";
+$LOCA["es"]["PR_ADMIRAL_INFO" ] = "Cantidad máxima de flotas +2";
+$LOCA["es"]["PR_ENGINEER_INFO" ] = "Minimiza las pérdidas de las defensas a la mitad<br> +10%  de producción de energía";
+$LOCA["es"]["PR_GEOLOGIST_INFO" ] = "+10% de producción de mineral";
+$LOCA["es"]["PR_TECHNO_INFO" ] = "+2 al nivel de espionaje, un 25 % menos de tiempo de investigación ";
 
-$LOCA["es"]["PR_PURCHASE" ] = "Order now!";
+$LOCA["es"]["PR_PURCHASE" ] = "¡Contrata ahora!";
 $LOCA["es"]["PR_RENEW" ] = "Renew!";   		// TODO: I'm not sure what it was exactly, since there is no HTML
-$LOCA["es"]["PR_ACTIVE_DAYS" ] = "Still &lt;font color=lime&gt;active&lt;/font&gt; for more than #1 days";
+$LOCA["es"]["PR_ACTIVE_DAYS" ] = "Aún &lt;font color=lime&gt;activo&lt;/font&gt; por #1 días más"; // Wanna give this a second pass afte comparing with original translation
 
 $LOCA["es"]["GLOBAL_ERROR"] = "Error";
 
