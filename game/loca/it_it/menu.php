c5709dccaedd2608aaa144c5e0b992087379ca4e
org <ogamespec@gmail.com>
2025-05-29 20:52:29 +0300

    it_it menu hotkeys

--- game/loca/it_it/menu.php
+++ game/loca/it_it/menu.php
@@ -31,4 +31,28 @@ $LOCA["it"]["MENU_RULES"] = "Regole";
 $LOCA["it"]["MENU_IMPRESSUM"] = "Impressum";
 $LOCA["it"]["MENU_DISCORD"] = "Discord"; 		// Nuovo. Il link a Discord viene inserito dopo il link alla bacheca.
 
+// Tasti di scelta rapida (introdotti nella versione 0.68a)
+
+$LOCA["it"]["HK_OVERVIEW"] = "o";
+$LOCA["it"]["HK_EMPIRE"] = "r";
+$LOCA["it"]["HK_BUILDING"] = "z";
+$LOCA["it"]["HK_RESOURCES"] = "s";
+$LOCA["it"]["HK_RESEARCH"] = "i";
+$LOCA["it"]["HK_SHIPYARD"] = "v";
+$LOCA["it"]["HK_FLEET"] = "f";
+$LOCA["it"]["HK_TECHTREE"] = "t";
+$LOCA["it"]["HK_GALAXY"] = "g";
+$LOCA["it"]["HK_DEFENSE"] = "x";
+$LOCA["it"]["HK_ALLY"] = "a";
+$LOCA["it"]["HK_BOARD"] = "m";
+$LOCA["it"]["HK_PAYMENT"] = "y"; 		// La traduzione originale dell'IF di 0,84 aveva “o” ed era in conflitto con la Panoramica (#77)
+$LOCA["it"]["HK_STAT"] = "k";
+$LOCA["it"]["HK_SEARCH"] = "p";
+$LOCA["it"]["HK_TUTORIAL"] = "^";
+$LOCA["it"]["HK_MESSAGES"] = "b";
+$LOCA["it"]["HK_NOTES"] = "e";
+$LOCA["it"]["HK_BUDDY"] = "d";
+$LOCA["it"]["HK_OPTIONS"] = "n";
+$LOCA["it"]["HK_LOGOUT"] = "q";
+
 ?>
