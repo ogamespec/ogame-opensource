df14665e364f00b29e1faa82c8cc8586a2f08478
org <ogamespec@gmail.com>
2025-06-28 16:17:27 +0300

    Feed implemented
    #84

---
+++ feed/viewitem.php
@@ -0,0 +1,70 @@
+<?php
+
+// Check if the configuration file is missing - exit
+if ( !file_exists ("../game/config.php"))
+{
+	exit ("Game not installed");
+}
+
+require_once "../game/config.php";
+require_once "../game/db.php";
+require_once "../game/utils.php";
+require_once "../game/uni.php";
+require_once "../game/user.php";
+
+InitDB();
+
+//print_r ($_REQUEST);
+
+if (!key_exists('feedid', $_REQUEST)) {
+	exit("No feed specified");
+}
+$feedid = $_REQUEST['feedid'];
+
+if (!key_exists('mid', $_REQUEST)) {
+	exit("No message specified");
+}
+$msg_id = $_REQUEST['mid'];
+
+// Check Universe settings
+
+$uni = LoadUniverse();
+if ($uni['feedage'] < 0) {
+	exit();
+}
+
+// Find user with specified feedid
+
+$query = "SELECT * FROM ".$db_prefix."users WHERE feedid = '".$feedid."' LIMIT 1";
+$result = dbquery ($query);
+if (dbrows($result) == 0) {
+	exit("Authentifizierung fehlgeschlagen");
+}
+$user = dbarray ($result);
+//print_r ($user);
+if (($user['flags'] & USER_FLAG_FEED_ENABLE) == 0) {
+	exit("Authentifizierung fehlgeschlagen");
+}
+$player_id = $user['player_id'];
+
+// Load the message and check that it is not later than the conditions for displaying Feed. Also check that the user is the owner of the message.
+
+$query = "SELECT * FROM ".$db_prefix."messages WHERE msg_id = $msg_id";
+$result = dbquery ($query);
+if ( !$result ) {
+	exit("No message");
+}
+$msg = dbarray ($result);
+//print_r ($msg);
+if ( $user['lastfeed'] != 0 && $msg['date'] > $user['lastfeed'] ) {
+	exit("The message cannot be viewed yet");
+}
+if ( $msg['owner_id'] != $player_id ) {
+	exit();
+}
+
+$subj = preg_replace('/<a[^>]*>(.*?)<\/a>/is', '$1', $msg['subj']);
+$text = preg_replace('/<a[^>]*>(.*?)<\/a>/is', '$1', $msg['text']);
+
+?>
+<html><head><title><?=$subj;?></title></head><body><h1><?=$subj;?></h1><p><?=$text;?></p><body></html>
