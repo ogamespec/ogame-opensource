a85ded22d5cb3e7a10d73dee7ab98e6befe9d8d5
ogamespec <ogamespec@gmail.com>
2026-01-09 17:36:07 +0300

    Update validate.php

--- game/validate.php
+++ game/validate.php
@@ -1,16 +1,17 @@
 <?php
 
+// Account activation via link.
+
 // Check if the configuration file is missing - redirect to the game installation page.
 if ( !file_exists ("config.php"))
 {
     echo "<html><head><meta http-equiv='refresh' content='0;url=install.php' /></head><body></body></html>";
-    ob_end_flush ();
     exit ();
 }
+else {
+    require_once "config.php";
+}
 
-// Account activation via link.
-
-require_once "config.php";
 require_once "db.php";
 require_once "utils.php";
 
