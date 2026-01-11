966fe6ff488a01e246a3bc7784554d2813562c68
org <ogamespec@gmail.com>
2026-01-11 22:07:53 +0300

    assets

---
+++ game/mods/SpaceStorm/main.php
@@ -0,0 +1,20 @@
+<?php
+
+// Space Storm mod.
+
+class SpaceStorm extends GameMod {
+
+    public function install() : void {
+
+    }
+
+    public function uninstall() : void {
+
+    }
+
+    public function init() : void {
+
+    }
+}
+
+?>
