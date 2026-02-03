707681b1063551b55255a7fa2752b66b56c8cf4c
ogamespec <ogamespec@gmail.com>
2026-02-03 20:50:53 +0300

    initial

---
+++ game/mods/Wanderer/main.php
@@ -0,0 +1,17 @@
+<?php
+
+// Wanderer Mod
+
+class Wanderer extends GameMod {
+
+    public function install() : void {
+    }
+
+    public function uninstall() : void {
+    }
+
+    public function init() : void {
+    }
+}
+
+?>
