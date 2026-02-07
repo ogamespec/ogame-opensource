cb232d233abe3ab103c116c9c60b1e182e528700
ogamespec <ogamespec@gmail.com>
2026-02-07 21:03:43 +0300

    Update admin_debug.php

--- game/pages_admin/admin_debug.php
+++ game/pages_admin/admin_debug.php
@@ -4,75 +4,71 @@
 
 class Admin_Debug extends Page {
 
+    private mixed $result;
+
     public function controller () : bool {
+
+        global $db_prefix;
+        global $GlobalUser;
+
+        loca_add ( "messages", $GlobalUser['lang'] );
+
+        $message_limit = 50;
+
+        if ( key_exists ('filter', $_REQUEST) ) $filter = $_REQUEST['filter'];
+        else $filter = "";
+
+        if ( method () === "POST" && $filter === "" && $GlobalUser['admin'] >= 2 )
+        {
+            if ( $_POST['deletemessages'] === "deleteall" )
+            {
+                $query = "TRUNCATE TABLE ".$db_prefix."debug;";
+                dbquery ($query);
+            }
+            else
+            {
+                $query = "SELECT * FROM ".$db_prefix."debug ORDER BY date DESC, error_id DESC LIMIT " . $message_limit;
+                $result = dbquery ($query);
+                $rows = dbrows ($result);
+                while ($rows--)
+                {
+                    $msg = dbarray ( $result );
+                    if ( key_exists ("delmes".$msg['error_id'], $_POST) || $_POST['deletemessages'] === "deleteshown" )
+                    {
+                        $query = "DELETE FROM ".$db_prefix."debug WHERE error_id = " . $msg['error_id'];
+                        dbquery ($query);
+                    }
+                }
+            }
+        }
+
+        if ( $filter === "" )
+        {
+            $query = "SELECT * FROM ".$db_prefix."debug ORDER BY date DESC, error_id DESC LIMIT " . $message_limit;
+        }
+        else
+        {
+            $query = "SELECT * FROM ".$db_prefix."debug WHERE text LIKE '%".$filter."%' ORDER BY date DESC, error_id DESC LIMIT " . $message_limit;
+        }
+        $this->result = dbquery ($query);
+
         return true;
     }
 
     public function view () : void {
-    }
-}
-
-function Admin_Debug () : void
-{
-    global $session;
-    global $db_prefix;
-    global $GlobalUser;
-
-    loca_add ( "messages", $GlobalUser['lang'] );
-
-    $message_limit = 50;
-
-    if ( key_exists ('filter', $_REQUEST) ) $filter = $_REQUEST['filter'];
-    else $filter = "";
-
-    if ( method () === "POST" && $filter === "" && $GlobalUser['admin'] >= 2 )
-    {
-        if ( $_POST['deletemessages'] === "deleteall" )
-        {
-            $query = "TRUNCATE TABLE ".$db_prefix."debug;";
-            dbquery ($query);
-        }
-        else
-        {
-            $query = "SELECT * FROM ".$db_prefix."debug ORDER BY date DESC, error_id DESC LIMIT " . $message_limit;
-            $result = dbquery ($query);
-            $rows = dbrows ($result);
-            while ($rows--)
-            {
-                $msg = dbarray ( $result );
-                if ( key_exists ("delmes".$msg['error_id'], $_POST) || $_POST['deletemessages'] === "deleteshown" )
-                {
-                    $query = "DELETE FROM ".$db_prefix."debug WHERE error_id = " . $msg['error_id'];
-                    dbquery ($query);
-                }
-            }
-        }
-    }
-
-    if ( $filter === "" )
-    {
-        $query = "SELECT * FROM ".$db_prefix."debug ORDER BY date DESC, error_id DESC LIMIT " . $message_limit;
-    }
-    else
-    {
-        $query = "SELECT * FROM ".$db_prefix."debug WHERE text LIKE '%".$filter."%' ORDER BY date DESC, error_id DESC LIMIT " . $message_limit;
-    }
-    $result = dbquery ($query);
+        global $session;
 
 ?>
-
-<?php AdminPanel();?>
-
 <table class='header'><tr class='header'><td><table width="519">
 <form action="index.php?page=admin&session=<?=$session;?>&mode=Debug" method="POST">
 <tr><td colspan="4" class="c"><?=loca("ADM_MSG_TITLE");?></td></tr>
 <tr><th><?=loca("ADM_MSG_ACTION");?></th><th><?=loca("ADM_MSG_DATE");?></th><th><?=loca("ADM_MSG_FROM");?></th><th><?=loca("ADM_MSG_BROWSER");?></th></tr>
 
 <?php
-    $rows = dbrows ($result);
+    $rows = dbrows ($this->result);
     while ($rows--) 
     {
-        $msg = dbarray ( $result );
+        $msg = dbarray ( $this->result );
         $user = LoadUser ($msg['owner_id']);
         $from = "<a href=\"index.php?page=admin&session=$session&mode=Users&player_id=".$msg['owner_id']."\">" . $user['oname'] . "</a> [" . $msg['ip'] . "]";
         $msg['text'] = str_replace ( "{PUBLIC_SESSION}", $session, $msg['text']);
@@ -97,6 +93,8 @@ function Admin_Debug () : void
 </table>
 
 <?php
+
+    }
 }
 
 ?>
