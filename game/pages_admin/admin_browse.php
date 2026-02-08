bd62c59de34bf3d53b20c5c39bd9a4e901bf268d
ogamespec <ogamespec@gmail.com>
2026-02-08 11:26:15 +0300

    Update admin_browse.php

--- game/pages_admin/admin_browse.php
+++ game/pages_admin/admin_browse.php
@@ -9,41 +9,38 @@ class Admin_Browse extends Page {
     }
 
     public function view () : void {
-    }
-}
 
-function Admin_Browse () : void
-{
-    global $session;
-    global $db_prefix;
-    global $GlobalUser;
+        global $session;
+        global $db_prefix;
+        global $GlobalUser;
 
-    $max_records = 50;
-    $query = "SELECT * FROM ".$db_prefix."browse ORDER BY date DESC LIMIT $max_records";
-    $result = dbquery ($query);
+        $max_records = 50;
+        $query = "SELECT * FROM ".$db_prefix."browse ORDER BY date DESC LIMIT $max_records";
+        $result = dbquery ($query);
 
-    AdminPanel();
-
-    $rows = dbrows ($result);
-    echo va(loca("ADM_BROWSE_TITLE"), $max_records) . "<br>";
-    echo "<table>\n";
-    while ($rows--) 
-    {
-        $log = dbarray ( $result );
-        $user = LoadUser ( $log['owner_id'] );
+        $rows = dbrows ($result);
+        echo va(loca("ADM_BROWSE_TITLE"), $max_records) . "<br>";
+        echo "<table>\n";
+        while ($rows--) 
+        {
+            $log = dbarray ( $result );
+            $user = LoadUser ( $log['owner_id'] );
 ?>
-        <tr><td><table>
-        <tr> <th> <?=$user['oname'];?> </th> <th> <?=$log['url'];?> </th> </tr>
-        <tr> <th rowspan=2>
-        <?=$log['method'];?><br>
-        <?=date ("d M Y", $log['date']);?><br>
-        <?=date ("H:i:s", $log['date']);?>
-        </th> <th> <?php print_r( unserialize($log['getdata']) );?> </th> </tr>
-        <tr> <th> <?php print_r( unserialize($log['postdata']) );?> </th> </tr>
-        </table></td></tr>
+            <tr><td><table>
+            <tr> <th> <?=$user['oname'];?> </th> <th> <?=$log['url'];?> </th> </tr>
+            <tr> <th rowspan=2>
+            <?=$log['method'];?><br>
+            <?=date ("d M Y", $log['date']);?><br>
+            <?=date ("H:i:s", $log['date']);?>
+            </th> <th> <?php print_r( unserialize($log['getdata']) );?> </th> </tr>
+            <tr> <th> <?php print_r( unserialize($log['postdata']) );?> </th> </tr>
+            </table></td></tr>
 
 <?php
+        }
+        echo "</table>\n";
+
     }
-    echo "</table>\n";
 }
+
 ?>
