ba9de6dd935b7bab1287ea1b9dd59ce6ca6a98c0
org <ogamespec@gmail.com>
2026-02-08 15:15:19 +0300

    Update admin_battle.php

--- game/pages_admin/admin_battle.php
+++ game/pages_admin/admin_battle.php
@@ -6,74 +6,62 @@
 class Admin_BattleReport extends Page {
 
     public function controller () : bool {
+        global $session;
+        global $db_prefix;
+        global $GlobalUser;
+
+        // Show battle report
+        if ( key_exists ( 'bericht', $_GET ) ) {
+            $query = "SELECT * FROM ".$db_prefix."battledata WHERE battle_id = " . intval ($_GET['bericht']);
+            $result = dbquery ( $query );
+            $row = dbarray ($result);
+
+            loca_add ( "battlereport", $GlobalUser['lang'] );
+    ?>
+    <html>
+    <HEAD>
+    <LINK rel="stylesheet" type="text/css" href="<?=UserSkin();?>formate.css">
+      <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+      <TITLE><?=loca("BATTLE_REPORT");?></TITLE>
+      <script src="js/utilities.js" type="text/javascript"></script>
+      <script type="text/javascript" src="js/overLib/overlib.js"></script>
+      <script language="JavaScript">var session="<?=$session;?>";</script>
+
+    </HEAD>
+    <BODY>
+    <div id="overDiv" style="position:absolute; visibility:hidden; z-index:1000;"></div>
+    <table width="99%">
+       <tr>
+        <td>
+    <?php
+            echo $row['report'];
+    ?>
+    </td>
+       </tr>
+    </table>
+    </BODY>
+    </html>
+    <?php
+            return false;
+        }
+
         return true;
     }
 
     public function view () : void {
-    }
-}
+        global $session;
+        global $db_prefix;
 
-function Admin_BattleReport () : void
-{
-    global $session;
-    global $db_prefix;
-    global $GlobalUser;
-    global $GlobalUni;
-
-?>
-
-<?php AdminPanel();?>
-
-<?php
-
-    // Show battle report
-    if ( key_exists ( 'bericht', $_GET ) ) {
-        $query = "SELECT * FROM ".$db_prefix."battledata WHERE battle_id = " . intval ($_GET['bericht']);
+        // Display a list of all reports
+        $query = "SELECT * FROM ".$db_prefix."battledata ORDER BY date DESC";
         $result = dbquery ( $query );
-        $row = dbarray ($result);
-        ob_clean ();
-        loca_add ( "battlereport", $GlobalUser['lang'] );
-?>
-<html>
-<HEAD>
-<LINK rel="stylesheet" type="text/css" href="<?=UserSkin();?>formate.css">
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <TITLE><?=loca("BATTLE_REPORT");?></TITLE>
-  <script src="js/utilities.js" type="text/javascript"></script>
-  <script type="text/javascript" src="js/overLib/overlib.js"></script>
-  <script language="JavaScript">var session="<?=$session;?>";</script>
 
-</HEAD>
-<BODY>
-<div id="overDiv" style="position:absolute; visibility:hidden; z-index:1000;"></div>
-<table width="99%">
-   <tr>
-    <td>
-<?php
-        echo $row['report'];
-?>
-</td>
-   </tr>
-</table>
-</BODY>
-</html>
-<?php
-        ob_end_flush ();
-        die ();
+        echo "<table>";
+        while ( $row = dbarray ($result) ) {
+            echo "<tr><td>".date("Y.m.d H:i:s", $row['date'])."</td><td>".str_replace ( "{PUBLIC_SESSION}" , $session, $row['title'])."</td></tr>";
+        }
+        echo "</table>";
     }
-
-    // Display a list of all reports
-    $query = "SELECT * FROM ".$db_prefix."battledata ORDER BY date DESC";
-    $result = dbquery ( $query );
-
-    echo "<table>";
-    while ( $row = dbarray ($result) ) {
-        echo "<tr><td>".date("Y.m.d H:i:s", $row['date'])."</td><td>".str_replace ( "{PUBLIC_SESSION}" , $session, $row['title'])."</td></tr>";
-    }
-    echo "</table>";
-
-?>
-
-<?php
 }
+
 ?>
